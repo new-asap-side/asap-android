0d0f27b60e852bef51737580822f1ba5f2d52e81
dev-JongH <dev.whdgus090@gmail.com>
2024-10-07 23:12:26 +0900

    Design: add Color palette

--- app/src/main/java/com/asap/aljyo/ui/theme/Color.kt
+++ app/src/main/java/com/asap/aljyo/ui/theme/Color.kt
@@ -2,5 +2,26 @@ package com.asap.aljyo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Primary color
 val Red01 = Color(0xFFFD2F7A)
 val Red02 = Color(0xFFFFEEF3)
+
+// Basic
+val Black01 = Color(0xFF222222)
+val Black02 = Color(0xFF666666)
+val Black03 = Color(0xFF888888)
+val Black04 = Color(0xFFB8B8B8)
+
+// Grey scale
+val Title = Color(0xFFF2F2F2)
+val Body01 = Color(0xFFE3E3E3)
+val Body02 = Color(0xFFCCCCCC)
+val Disable = Color(0xFFAAAAAA)
+
+// Additional
+val Error = Color(0xFFDE0000)
+
+// Icon
+val Blue = Color(0xFF0066FF)
+val Green = Color(0xFF17C07E)
+val Yellow = Color(0xFFFFC441)

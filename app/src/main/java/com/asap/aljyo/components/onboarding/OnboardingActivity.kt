715b6095d04ac5921a4e8b1d09635ea46ea50200
dev-JongH <dev.whdgus090@gmail.com>
2024-10-08 15:44:07 +0900

    Design: OnboardingActivity statusBar color 적용

--- app/src/main/java/com/asap/aljyo/components/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/asap/aljyo/components/onboarding/OnboardingActivity.kt
@@ -2,14 +2,24 @@ package com.asap.aljyo.components.onboarding
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.toArgb
 import com.asap.aljyo.ui.composable.onboarding.OnboardingPager
+import com.asap.aljyo.ui.theme.White
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OnboardingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                White.toArgb(),
+                White.toArgb()
+            )
+        )
         setContent {
             OnboardingPager()
         }

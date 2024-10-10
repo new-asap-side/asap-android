7fc0b3b7af0315227f6727712921e689ab352a38
dev-JongH <dev.whdgus090@gmail.com>
2024-10-10 15:36:09 +0900

    Feat: Onboarding Main composable 추가

---
+++ app/src/main/java/com/asap/aljyo/ui/composable/onboarding/OnboardingContent.kt
@@ -0,0 +1,37 @@
+package com.asap.aljyo.ui.composable.onboarding
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.asap.aljyo.R
+
+@Composable
+fun OnboardingMain() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        Image(
+            painterResource(R.drawable.onboarding_main_title),
+            contentDescription = "onboarding main title"
+        )
+        Image(
+            painterResource(R.drawable.onboarding_main_image),
+            contentDescription = "onboarding main title"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnboardingMainPreview() {
+    OnboardingMain()
+}

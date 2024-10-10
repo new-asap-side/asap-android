ac174aa2c2233971a2f05fd432896b6a904240cc
dev-JongH <dev.whdgus090@gmail.com>
2024-10-10 21:36:35 +0900

    Fix: Onboarding Main fillMaxWidth 적용

--- app/src/main/java/com/asap/aljyo/ui/composable/onboarding/OnboardingContent.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/onboarding/OnboardingContent.kt
@@ -3,8 +3,10 @@ package com.asap.aljyo.ui.composable.onboarding
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,6 +15,7 @@ import com.asap.aljyo.R
 @Composable
 fun OnboardingMain() {
     Column(
+        modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {

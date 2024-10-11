9914a1c54f21b3ad3d0c070d59fc96d9503ad073
dev-JongH <dev.whdgus090@gmail.com>
2024-10-11 17:44:44 +0900

    Design: KakaoLoginButton UI

--- app/src/main/java/com/asap/aljyo/components/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/asap/aljyo/components/onboarding/OnboardingActivity.kt
@@ -5,10 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
 import com.asap.aljyo.ui.composable.onboarding.OnboardingPager
+import com.asap.aljyo.ui.composable.onboarding.SocialLogin
 import com.asap.aljyo.ui.theme.AljyoTheme
 import com.asap.aljyo.ui.theme.White
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,9 +32,25 @@ class OnboardingActivity : ComponentActivity() {
         )
         setContent {
             AljyoTheme {
-                OnboardingPager(
-                    modifier = Modifier.fillMaxSize()
-                )
+                Scaffold { innerPadding ->
+                    Box(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        OnboardingPager(
+                            modifier = Modifier.fillMaxSize()
+                        )
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.BottomStart
+                        ) {
+                            SocialLogin(
+                                modifier = Modifier
+                                    .padding(horizontal = 20.dp, vertical = 24.dp)
+                                    .fillMaxWidth()
+                            )
+                        }
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/asap/aljyo/ui/composable/onboarding/SocialLogin.kt
@@ -0,0 +1,70 @@
+package com.asap.aljyo.ui.composable.onboarding
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.asap.aljyo.R
+import com.asap.aljyo.ui.theme.AljyoTheme
+
+@Composable
+fun SocialLogin(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        KakaoLoginButton()
+    }
+}
+
+@Composable
+fun KakaoLoginButton() {
+    TextButton(
+        onClick = {},
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFFEE400),
+            contentColor = Color(0xFF371D1E)
+        ),
+        modifier = Modifier.height(52.dp)
+    ) {
+        Icon(
+            painterResource(R.drawable.ic_kakao),
+            contentDescription = "kakao icon"
+        )
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .offset((-10).dp),
+            text = stringResource(R.string.start_with_kakao),
+            style = MaterialTheme.typography.bodyMedium.copy(
+                color = Color.Black,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 300, heightDp = 50)
+@Composable
+fun KakaoLoginButtonPreview() {
+    AljyoTheme {
+        KakaoLoginButton()
+    }
+}

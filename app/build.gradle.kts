c1e05ddd7d84b0dd126254cffe5d31fa41eaa772
dev-JongH <dev.whdgus090@gmail.com>
2024-10-14 20:51:18 +0900

    Chore: add data module dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // module
+    implementation(project(":data"))
 }

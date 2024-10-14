f23040c29c9eb11d6ee7ccc1eff9a95503f03cd9
dev-JongH <dev.whdgus090@gmail.com>
2024-10-14 20:46:55 +0900

    Feat: Create NetworkModule

---
+++ data/src/main/java/com/asap/data/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.asap.data.di
+
+import com.asap.data.network.service.LoginService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+internal const val baseUrl = "http://aljo.shop/docs"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideLoginApiService(retrofit: Retrofit): LoginService {
+        return retrofit.create(LoginService::class.java)
+    }
+}

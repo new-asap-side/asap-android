60e6557c68d1c53e9eec9a9da0410e29897fc285
dev-JongH <dev.whdgus090@gmail.com>
2024-10-14 20:54:02 +0900

    Fix: kap plugin 선언부 최하단으로 이동

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {

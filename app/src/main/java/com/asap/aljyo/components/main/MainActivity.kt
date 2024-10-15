4065aeee30a85b3816fae1951705599758236080
dev-JongH <dev.whdgus090@gmail.com>
2024-10-15 13:44:53 +0900

    Design: MainActivity system bar color [ASAP-55]

--- app/src/main/java/com/asap/aljyo/components/main/MainActivity.kt
+++ app/src/main/java/com/asap/aljyo/components/main/MainActivity.kt
@@ -2,13 +2,23 @@ package com.asap.aljyo.components.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.toArgb
+import com.asap.aljyo.ui.theme.White
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                White.toArgb(),
+                White.toArgb()
+            )
+        )
         setContent {
 
         }

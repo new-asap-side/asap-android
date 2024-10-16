7c05fa66517638139b17564e474c105f39a993a5
dev-JongH <dev.whdgus090@gmail.com>
2024-10-16 10:53:44 +0900

    Style: MainActivity import 정리

--- app/src/main/java/com/asap/aljyo/components/main/MainActivity.kt
+++ app/src/main/java/com/asap/aljyo/components/main/MainActivity.kt
@@ -7,14 +7,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsBottomHeight
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb

05892ddc3fa1bed4345dacb4d6ac481e2c84455e
dev-JongH <dev.whdgus090@gmail.com>
2024-10-19 14:50:29 +0900

    Feat: create UserRepositoryImpl [ASAP-55]

---
+++ data/src/main/java/com/asap/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.asap.data.repository
+
+import com.asap.domain.entity.ResultCard
+import com.asap.domain.repository.UserInfoRepository
+import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserRepositoryImpl @Inject constructor(
+
+) : UserInfoRepository {
+    override suspend fun kakaoLogin(): Response<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchResultCardData(): Response<ResultCard> {
+        TODO("Not yet implemented")
+    }
+}

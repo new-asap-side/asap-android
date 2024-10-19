08c38e852e39f5cda54c35564ca4e6d5ba21018d
dev-JongH <dev.whdgus090@gmail.com>
2024-10-19 16:56:41 +0900

    Rename: UserInfoRepository -> UserRepository [ASAP-55]

--- domain/src/main/java/com/asap/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/asap/domain/repository/UserRepository.kt
@@ -3,7 +3,7 @@ package com.asap.domain.repository
 import com.asap.domain.entity.ResultCard
 import retrofit2.Response
 
-interface UserInfoRepository {
+interface UserRepository {
     suspend fun kakaoLogin(): Response<Unit>
 
     suspend fun fetchResultCardData(): Response<ResultCard>

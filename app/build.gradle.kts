6462197df16b19d025181cc89be0c86fb8a3ca95
dev-JongH <dev.whdgus090@gmail.com>
2024-10-19 16:55:46 +0900

    Chore: app module navigation compose dependency [ASAP-55]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.core.splashscreen)
     /// hilt
     implementation(libs.hilt)
+    implementation(libs.hilt.navigation.compose)
     kapt(libs.hilt.compiler)
     // navigation with compose
     implementation(libs.navigation.compose)
@@ -82,5 +83,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     // module
+    implementation(project(":domain"))
     implementation(project(":data"))
 }

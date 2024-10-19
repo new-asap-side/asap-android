61e9fadce3f2f5731f66cd58fad4a479b04d729c
dev-JongH <dev.whdgus090@gmail.com>
2024-10-19 14:50:41 +0900

    Feat: create UserService [ASAP-55]

---
+++ data/src/main/java/com/asap/data/remote/service/UserService.kt
@@ -0,0 +1,14 @@
+package com.asap.data.remote.service
+
+import com.asap.domain.entity.ResultCard
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface UserService {
+    @GET("/auth/kakao-login-page")
+    suspend fun kakaoLogin(): Response<Unit>
+
+    @GET("/")
+    suspend fun fetchResultCard(): Response<ResultCard>
+}
+

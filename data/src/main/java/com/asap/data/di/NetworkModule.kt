e2ab32e9fb8549d1e4aefbb24915e8b0844e2a24
dev-JongH <dev.whdgus090@gmail.com>
2024-10-28 14:24:36 +0900

    Feat: KakaoLogin API 연동 [ASAP-43]

--- data/src/main/java/com/asap/data/di/NetworkModule.kt
+++ data/src/main/java/com/asap/data/di/NetworkModule.kt
@@ -11,7 +11,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
-internal const val baseUrl = "http://aljo.shop/docs/"
+internal const val baseUrl = "http://aljo.shop/"
 
 @Module
 @InstallIn(SingletonComponent::class)
--- data/src/main/java/com/asap/data/remote/service/UserService.kt
+++ data/src/main/java/com/asap/data/remote/service/UserService.kt
@@ -4,9 +4,11 @@ import com.asap.domain.entity.KakaoLoginResponse
 import com.asap.domain.entity.ResultCard
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface UserService {
-    @GET("/auth/kakao-login-page")
+    @Headers("Content-Type: application/json")
+    @GET("/auth/kakao")
     suspend fun kakaoLogin(): Response<KakaoLoginResponse>
 
     @GET("/")

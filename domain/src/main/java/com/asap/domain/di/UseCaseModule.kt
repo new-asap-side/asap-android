cee4b24f6d8be679d2f32c3cef991889894bb8df
dev-JongH <dev.whdgus090@gmail.com>
2024-10-28 11:16:36 +0900

    Feat: DI KakaoLoginUseCase [ASAP-43]

--- domain/src/main/java/com/asap/domain/di/UseCaseModule.kt
+++ domain/src/main/java/com/asap/domain/di/UseCaseModule.kt
@@ -1,5 +1,7 @@
 package com.asap.domain.di
 
+import com.asap.domain.usecase.KakaoLoginUseCase
+import com.asap.domain.usecase.KakaoLoginUseCaseImpl
 import com.asap.domain.usecase.ResultCardUseCase
 import com.asap.domain.usecase.ResultCardUseCaseImpl
 import dagger.Binds
@@ -10,6 +12,11 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 interface UseCaseModule {
+    @Binds
+    fun bindKakaoLoginUseCase(
+        kakaoLoginUseCaseImpl: KakaoLoginUseCaseImpl
+    ): KakaoLoginUseCase
+
     @Binds
     fun bindResultCardUseCase(
         resultCardUseCaseImpl: ResultCardUseCaseImpl

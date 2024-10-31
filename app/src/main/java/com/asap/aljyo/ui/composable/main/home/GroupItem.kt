a3b84edd0d6e9ec6b2e5cfb5d309a3abf23cfccf
dev-JongH <dev.whdgus090@gmail.com>
2024-10-31 17:35:03 +0900

    Feat: GroupItem 그룹명, 요일, 시간 추가 [ASAP-55]

--- app/src/main/java/com/asap/aljyo/ui/composable/main/home/GroupItem.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/home/GroupItem.kt
@@ -1,10 +1,12 @@
 package com.asap.aljyo.ui.composable.main.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,20 +20,49 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.asap.aljyo.R
 import com.asap.aljyo.ui.theme.AljyoTheme
+import com.asap.aljyo.ui.theme.Black01
+import com.asap.aljyo.ui.theme.Black02
 import com.asap.aljyo.ui.theme.Grey01
 
+private val dummyDates = listOf("월", "화", "수", "목", "금", "토", "일")
+private const val everyDay = 7
+
 @Composable
-fun GroupItem() {
-    Column {
+fun GroupItem(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
         GroupThumbnail()
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            "그룹 타이틀그룹 타이틀그룹 타이틀그룹 타이틀그룹 타이틀",
+            style = MaterialTheme.typography.bodyMedium.copy(
+                fontSize = 14.sp,
+                color = Black01
+            ),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            val dates = if (dummyDates.size == everyDay)
+                stringResource(R.string.everyday)
+            else dummyDates.joinToString(
+                separator = " "
+            )
+            GreyBackgroundText(dates)
+            GreyBackgroundText("21:00")
+        }
     }
 }
 
@@ -39,7 +70,7 @@ fun GroupItem() {
 @Composable
 fun GroupItemPreview() {
     AljyoTheme {
-        GroupItem()
+        GroupItem(modifier = Modifier.width(148.dp))
     }
 }
 
@@ -50,7 +81,10 @@ fun GroupThumbnail() {
     ) {
         AsyncImage(
             model = "",
-            contentDescription = "Group thumbnail"
+            modifier = Modifier.size(148.dp),
+            contentScale = ContentScale.FillBounds,
+            contentDescription = "Group thumbnail",
+            error = painterResource(R.drawable.ic_my_page)
         )
         Row(
             modifier = Modifier
@@ -60,20 +94,48 @@ fun GroupThumbnail() {
                 .padding(4.dp, 2.dp, 6.dp, 2.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            val publicGroup = false
+            val painter = if (publicGroup) painterResource(R.drawable.ic_hello)
+            else painterResource(R.drawable.ic_lock)
+
+            val text = if (publicGroup) stringResource(R.string.public_group)
+            else stringResource(R.string.private_group)
+
             Icon(
                 modifier = Modifier.size(20.dp),
-                painter = painterResource(R.drawable.ic_hello),
+                painter = painter,
                 contentDescription = "Public group icon",
                 tint = Color.Unspecified
             )
             Spacer(modifier = Modifier.width(2.dp))
             Text(
-                stringResource(R.string.public_group),
+                text,
                 style = MaterialTheme.typography.bodyMedium.copy(
                     fontSize = 12.sp,
                     color = Grey01
                 )
             )
         }
+    }
+}
+
+@Composable
+fun GreyBackgroundText(text: String) {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(Grey01)
+            .padding(
+                horizontal = 6.dp,
+                vertical = 2.dp
+            )
+    ) {
+        Text(
+            text,
+            style = MaterialTheme.typography.bodyMedium.copy(
+                fontSize = 12.sp,
+                color = Black02
+            )
+        )
     }
 }

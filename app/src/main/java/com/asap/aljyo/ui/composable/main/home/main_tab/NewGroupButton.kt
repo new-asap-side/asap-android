314ca8793cf62aaf5bdc62005a8bfcad4024ac0b
dev-JongH <dev.whdgus090@gmail.com>
2024-11-04 16:57:20 +0900

    Design: NewGroupButton composable 추가 [ASAP-51]

---
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/home/main_tab/NewGroupButton.kt
@@ -0,0 +1,41 @@
+package com.asap.aljyo.ui.composable.main.home.main_tab
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.FloatingActionButtonElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.asap.aljyo.R
+import com.asap.aljyo.ui.theme.AljyoTheme
+import com.asap.aljyo.ui.theme.White
+
+@Composable
+fun NewGroupButton() {
+    FloatingActionButton(
+        contentColor = White,
+        containerColor = MaterialTheme.colorScheme.primary,
+        shape = CircleShape,
+        elevation = FloatingActionButtonDefaults.elevation(
+            defaultElevation = 5.dp
+        ),
+        onClick = {}
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_new_group),
+            contentDescription = "New group icon"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NewGroupButtonPreview() {
+    AljyoTheme {
+        NewGroupButton()
+    }
+}

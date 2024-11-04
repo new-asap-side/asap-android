26abd1334204999f0c6d9cc718f5637d8f8ec6ef
dev-JongH <dev.whdgus090@gmail.com>
2024-11-04 15:46:51 +0900

    Design: Banner composable 추가 [ASAP-55]

--- app/src/main/java/com/asap/aljyo/ui/composable/main/home/main_tab/HomeMainScreen.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/home/main_tab/HomeMainScreen.kt
@@ -3,11 +3,14 @@ package com.asap.aljyo.ui.composable.main.home.main_tab
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.asap.aljyo.ui.theme.Grey01
 import com.asap.aljyo.ui.theme.White
@@ -29,6 +32,13 @@ fun HomeMainScreen() {
         )
         TodayPopularGroup(
             modifier = Modifier.background(color = White)
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        Banner(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .clip(RoundedCornerShape(10.dp)),
         )
     }
 }

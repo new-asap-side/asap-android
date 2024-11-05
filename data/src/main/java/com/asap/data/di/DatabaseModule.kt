bfc85fbe40204aa26cad482a0d789164f4a2304f
KJ-Hyeon <bark052526@gmail.com>
2024-11-05 16:46:07 +0900

    Feat: Add Hilt dependency injection for DataStore [ASAP-47]

--- data/src/main/java/com/asap/data/di/DatabaseModule.kt
+++ data/src/main/java/com/asap/data/di/DatabaseModule.kt
@@ -1,6 +1,9 @@
 package com.asap.data.di
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.room.Room
 import com.asap.data.local.AppDatabase
 import com.asap.data.local.MIGRATION_1_2
@@ -10,6 +13,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import androidx.datastore.preferences.core.Preferences
+import com.asap.data.BuildConfig
+
 import javax.inject.Singleton
 
 
@@ -25,4 +31,12 @@ object DatabaseModule {
     @Provides
     @Singleton
     fun provideUserDao(appDatabase: AppDatabase): UserDao = appDatabase.userDao()
+
+    @Provides
+    @Singleton
+    fun provideDateStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create (
+            produceFile = { context.preferencesDataStoreFile(BuildConfig.TOKEN_DATASTORE_NAME) }
+        )
+    }
 }

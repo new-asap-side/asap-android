4aa74bf94563fee5a03796a1bbbd8d6bd1ffed89
KJ-Hyeon <bark052526@gmail.com>
2024-11-06 14:17:18 +0900

    Feat: Add SaveProfile Response & Request Model [ASAP-47]

---
+++ data/src/main/java/com/asap/data/remote/request/SaveProfileRequest.kt
@@ -0,0 +1,13 @@
+package com.asap.data.remote.request
+
+import com.google.gson.annotations.SerializedName
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SaveProfileRequest(
+    val userId: Int,
+    val nickName: String,
+    @Json(name = "profile_img")
+    val profileImg: String
+)

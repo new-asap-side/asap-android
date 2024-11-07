3d6930f1025b8403db72728985d385fad7fc577a
dev-JongH <dev.whdgus090@gmail.com>
2024-11-07 14:55:45 +0900

    Feat: apply AlarmGroup parcelable [ASAP-5]

--- domain/src/main/java/com/asap/domain/entity/remote/AlarmGroup.kt
+++ domain/src/main/java/com/asap/domain/entity/remote/AlarmGroup.kt
@@ -1,9 +1,13 @@
 package com.asap.domain.entity.remote
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = true)
+@Parcelize
 data class AlarmGroup(
     @Json(name = "isPublic")
     val isPublic: Boolean,
@@ -19,7 +23,31 @@ data class AlarmGroup(
     val totalNumber: Int,
     @Json(name = "currentNumber")
     val currentNumber: Int,
-) {
+) : Parcelable {
+    constructor (parcel: Parcel) : this(
+        isPublic = parcel.readInt() == 1,
+        thumbnailUrl = parcel.readString() ?: "",
+        title = parcel.readString() ?: "",
+        alarmDates = parcel.createStringArrayList() ?: listOf(),
+        alarmTime = parcel.readString() ?: "",
+        totalNumber = parcel.readInt(),
+        currentNumber = parcel.readInt(),
+    )
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        with(dest) {
+            writeInt(if (isPublic) 1 else 0)
+            writeString(thumbnailUrl)
+            writeString(title)
+            writeStringList(alarmDates)
+            writeString(alarmTime)
+            writeInt(totalNumber)
+            writeInt(currentNumber)
+        }
+    }
+
     companion object {
         fun dummy(): AlarmGroup {
             return AlarmGroup(
@@ -31,6 +59,17 @@ data class AlarmGroup(
                 totalNumber = 8,
                 currentNumber = 4,
             )
+        }
+
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<AlarmGroup> {
+            override fun createFromParcel(parcel: Parcel): AlarmGroup {
+                return AlarmGroup(parcel)
+            }
+
+            override fun newArray(size: Int): Array<AlarmGroup?> {
+                return arrayOfNulls(size)
+            }
         }
     }
 }

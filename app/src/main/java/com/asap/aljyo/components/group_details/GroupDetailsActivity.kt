1788c7c42c1001f95cbb2b73f536fdc6fd05ae9b
dev-JongH <dev.whdgus090@gmail.com>
2024-11-07 16:34:31 +0900

    Feat: add GroupDetailsActivity TopAppBar (dummy) [ASAP-5]

--- app/src/main/java/com/asap/aljyo/components/group_details/GroupDetailsActivity.kt
+++ app/src/main/java/com/asap/aljyo/components/group_details/GroupDetailsActivity.kt
@@ -6,13 +6,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
 import com.asap.aljyo.components.ExtrasKey
+import com.asap.aljyo.ui.composable.group_details.NextAlarmTimer
 import com.asap.aljyo.ui.theme.AljyoTheme
 import com.asap.aljyo.ui.theme.White
 import com.asap.domain.entity.remote.AlarmGroup
@@ -29,6 +43,7 @@ class GroupDetailsActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
@@ -39,7 +54,50 @@ class GroupDetailsActivity : ComponentActivity() {
         )
         setContent {
             AljyoTheme {
-                Scaffold { padding ->
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    NextAlarmTimer(
+                                        modifier = Modifier
+                                            .clip(RoundedCornerShape(100))
+                                            .background(color = White.copy(alpha = 0.88f))
+                                            .padding(
+                                                vertical = 6.dp,
+                                                horizontal = 14.dp
+                                            )
+                                    )
+                                }
+                            },
+                            actions = {
+                                IconButton(
+                                    onClick = { }
+                                ) {
+                                    Icon(
+                                        Icons.Default.MoreVert,
+                                        tint = White,
+                                        contentDescription = "Top app bar action"
+                                    )
+                                }
+                            },
+                            navigationIcon = {
+                                IconButton(
+                                    onClick = { finish() }
+                                ) {
+                                    Icon(
+                                        Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                                        tint = White,
+                                        contentDescription = "Top app bar navigation icon"
+                                    )
+                                }
+                            }
+                        )
+                    }
+                ) { padding ->
                     Surface(
                         modifier = Modifier
                             .padding(bottom = padding.calculateBottomPadding())

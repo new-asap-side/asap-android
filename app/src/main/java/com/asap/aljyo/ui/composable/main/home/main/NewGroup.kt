f407dba5995c19342b36875998ac390e09d1c6d9
dev-JongH <dev.whdgus090@gmail.com>
2024-11-07 14:56:56 +0900

    Feat: putExtras AlarmGroup entity [ASAP-5]

--- app/src/main/java/com/asap/aljyo/ui/composable/main/home/main/NewGroup.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/home/main/NewGroup.kt
@@ -1,5 +1,6 @@
 package com.asap.aljyo.ui.composable.main.home.main
 
+import android.content.Intent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,16 +10,21 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.asap.aljyo.R
+import com.asap.aljyo.components.ExtrasKey
+import com.asap.aljyo.components.group_details.GroupDetailsActivity
 import com.asap.aljyo.ui.composable.main.home.GroupItem
+import com.asap.domain.entity.remote.AlarmGroup
 
 @Composable
 fun NewGroupList(
     modifier: Modifier = Modifier,
     tabChange: (Int) -> Unit,
 ) {
+    val context = LocalContext.current
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -38,7 +44,13 @@ fun NewGroupList(
         ) {
             items(6) {
                 GroupItem(
-                    modifier = Modifier.clickable {  }
+                    modifier = Modifier.clickable {
+                        Intent(context, GroupDetailsActivity::class.java).apply {
+                            putExtra(ExtrasKey.ALARM_GROUP_KEY, AlarmGroup.dummy())
+                        }.also {
+                            context.startActivity(it)
+                        }
+                    }
                 )
             }
         }
--- app/src/main/java/com/asap/aljyo/ui/composable/main/home/main/TodayPopularGroup.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/home/main/TodayPopularGroup.kt
@@ -1,5 +1,6 @@
 package com.asap.aljyo.ui.composable.main.home.main
 
+import android.content.Intent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
@@ -15,13 +16,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.asap.aljyo.R
+import com.asap.aljyo.components.ExtrasKey
+import com.asap.aljyo.components.group_details.GroupDetailsActivity
 import com.asap.aljyo.ui.composable.common.overscroll.CustomOverscroll
 import com.asap.aljyo.ui.composable.main.home.GroupItem
 import com.asap.aljyo.ui.theme.AljyoTheme
+import com.asap.domain.entity.remote.AlarmGroup
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -35,6 +40,7 @@ fun TodayPopularGroup(
         CustomOverscroll.HorizontalClamping(scope = coroutineScope)
     }
 
+    val context = LocalContext.current
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -62,7 +68,13 @@ fun TodayPopularGroup(
                 GroupItem(
                     modifier = Modifier
                         .width(148.dp)
-                        .clickable { }
+                        .clickable {
+                            Intent(context, GroupDetailsActivity::class.java).apply {
+                                putExtra(ExtrasKey.ALARM_GROUP_KEY, AlarmGroup.dummy())
+                            }.also {
+                                context.startActivity(it)
+                            }
+                        }
                 )
             }
         }

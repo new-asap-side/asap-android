ddb27a9f381d771f0f7cf1622e0e3cd32a9b25c6
dev-JongH <dev.whdgus090@gmail.com>
2024-11-07 15:46:55 +0900

    Feat: add GroupDetailsActivity UI frame [ASAP-5]

--- app/src/main/java/com/asap/aljyo/components/group_details/GroupDetailsActivity.kt
+++ app/src/main/java/com/asap/aljyo/components/group_details/GroupDetailsActivity.kt
@@ -2,11 +2,19 @@ package com.asap.aljyo.components.group_details
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import com.asap.aljyo.components.ExtrasKey
 import com.asap.aljyo.ui.theme.AljyoTheme
+import com.asap.aljyo.ui.theme.White
 import com.asap.domain.entity.remote.AlarmGroup
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,10 +31,22 @@ class GroupDetailsActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("GroupDetailsActivity", "$alarmGroupItem")
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                White.copy(alpha = 0f).toArgb(),
+                White.toArgb()
+            )
+        )
         setContent {
             AljyoTheme {
-
+                Scaffold { padding ->
+                    Surface(
+                        modifier = Modifier
+                            .padding(bottom = padding.calculateBottomPadding())
+                            .fillMaxSize(),
+                    ) {
+                    }
+                }
             }
         }
     }

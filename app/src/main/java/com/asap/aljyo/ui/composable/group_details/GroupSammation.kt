fbbaa9784dbd15efcebaefb8871f04f34bca1bce
dev-JongH <dev.whdgus090@gmail.com>
2024-11-07 20:18:47 +0900

    Feat: add GroupPersonnel composable (dummy) [ASAP-5]

--- app/src/main/java/com/asap/aljyo/ui/composable/group_details/GroupSammation.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/group_details/GroupSammation.kt
@@ -1,15 +1,19 @@
 package com.asap.aljyo.ui.composable.group_details
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -22,15 +26,21 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.asap.aljyo.R
+import com.asap.aljyo.ui.composable.common.custom.OverlappingRow
 import com.asap.aljyo.ui.theme.AljyoTheme
 import com.asap.aljyo.ui.theme.Black01
 import com.asap.aljyo.ui.theme.Grey01
+import com.asap.aljyo.ui.theme.Grey03
+import com.asap.aljyo.ui.theme.Red01
 import com.asap.aljyo.ui.theme.White
+import com.asap.aljyo.ui.theme.Yellow
 
 @Composable
 fun GroupSummation(
@@ -66,11 +76,50 @@ fun GroupSummation(
                 color = Grey01,
                 thickness = 1.dp
             )
+            Spacer(modifier = Modifier.height(18.dp))
+            Text(
+                text = "GROUP TITLE GROUP TITLE GROUP TITLE GROUP TITLE GROUP TITLE",
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontSize = 22.sp,
+                    color = Black01,
+                ),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+            GroupAlarmDates(
+                dates = "월 화 수",
+                timeStamp = "21:00"
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Desciption",
+                style = MaterialTheme.typography.labelMedium.copy(
+                    fontSize = 15.sp,
+                    color = Color(0xFF666666),
+                ),
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            GroupPersonnel(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8))
+                    .border(
+                        width = 1.dp,
+                        color = Color(0xFFFFD9E7)
+                    )
+                    .background(color = MaterialTheme.colorScheme.secondary)
+                    .padding(
+                        vertical = 13.dp,
+                        horizontal = 14.dp
+                    ),
+                personnel = 6
+            )
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 360)
+@Preview(showBackground = true, widthDp = 360, heightDp = 555)
 @Composable
 fun GroupSummationPreview() {
     AljyoTheme {
@@ -116,5 +165,112 @@ fun GroupLeader(modifier: Modifier = Modifier) {
 fun GroupLeaderPreview() {
     AljyoTheme {
         GroupLeader()
+    }
+}
+
+@Composable
+fun GroupAlarmDates(
+    modifier: Modifier = Modifier,
+    dates: String,
+    timeStamp: String
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_clock),
+            contentDescription = "clock icon"
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = dates,
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontSize = 16.sp,
+                color = Black01
+            )
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        VerticalDivider(
+            modifier = Modifier.height(14.dp),
+            color = Grey03,
+            thickness = 1.5.dp,
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        Text(
+            text = timeStamp,
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontSize = 16.sp,
+                color = Black01
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GroupAlarmDatesPreview() {
+    AljyoTheme {
+        GroupAlarmDates(dates = "월 화 수", timeStamp = "21:00")
+    }
+}
+
+@Composable
+fun GroupPersonnel(
+    modifier: Modifier = Modifier,
+    personnel: Int = 0,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                painter = painterResource(R.drawable.ic_my_page),
+                contentDescription = "Group personnel icon",
+                tint = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = stringResource(R.string.particularting_personnel, personnel),
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    fontSize = 14.sp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+        OverlappingRow {
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .background(color = Red01)
+            )
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .background(color = Yellow)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360)
+@Composable
+fun GroupPersonnelPreview() {
+    AljyoTheme {
+        GroupPersonnel(
+            modifier = Modifier
+                .clip(RoundedCornerShape(8))
+                .border(
+                    width = 1.dp,
+                    color = Color(0xFFFFD9E7)
+                )
+                .background(color = MaterialTheme.colorScheme.secondary)
+                .padding(
+                    vertical = 13.dp,
+                    horizontal = 14.dp
+                )
+        )
     }
 }

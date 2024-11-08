ce77288fec5d511bfdfcfecac94c3b473a1da637
KJ-Hyeon <bark052526@gmail.com>
2024-11-08 13:03:17 +0900

    Feat: Add Update profileImg Query  [ASAP-47]

--- data/src/main/java/com/asap/data/local/dao/UserDao.kt
+++ data/src/main/java/com/asap/data/local/dao/UserDao.kt
@@ -14,6 +14,9 @@ interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(vararg user: User)
 
+    @Query("UPDATE User SET profileImg = :profileImg WHERE user_id = :userId")
+    suspend fun updateProfileImg(profileImg: String?, userId: Int)
+
     @Query("SELECT * FROM User")
     suspend fun selectAll(): List<User>
 

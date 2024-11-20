6e7acb34593257ebef3edeaea1973ae7d756e5d4
dev-JongH <dev.whdgus090@gmail.com>
2024-11-20 16:27:27 +0900

    Feat: UnRankingArea user thumbnail clip circle [ASAP-91]

--- app/src/main/java/com/asap/aljyo/ui/composable/group_ranking/UnRankingArea.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/group_ranking/UnRankingArea.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,7 +55,9 @@ internal fun UnRankingArea(
                     horizontalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
                     AsyncImage(
-                        modifier = Modifier.size(36.dp),
+                        modifier = Modifier
+                            .size(36.dp)
+                            .clip(CircleShape),
                         model = ranking.thumbnail,
                         contentDescription = "Group particular thumbnail",
                         error = painterResource(R.drawable.ic_my_page)

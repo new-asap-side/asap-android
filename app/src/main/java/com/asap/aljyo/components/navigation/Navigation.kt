8d0e7894f5c8bc25c3560d12a4f07f97d0b0df79
dev-JongH <dev.whdgus090@gmail.com>
2024-12-02 16:38:46 +0900

    Feat: EmptyAlarmList UI [ASAP-92]

--- app/src/main/java/com/asap/aljyo/components/navigation/Navigation.kt
+++ app/src/main/java/com/asap/aljyo/components/navigation/Navigation.kt
@@ -26,8 +26,7 @@ internal fun AppNavHost() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-//        startDestination = ScreenRoute.Onboarding.route,
-        startDestination = ScreenRoute.ReleaseAlarm.route,
+        startDestination = ScreenRoute.Onboarding.route,
     ) {
         composable(route = ScreenRoute.Onboarding.route) {
             OnboardingScreen(
@@ -116,7 +115,15 @@ fun MainNavHost(
         }
 
         composable(route = MainScreenRoute.AlarmList.route) {
-            AlarmListScreen()
+            AlarmListScreen(
+                navigateToHome = {
+                    navController.navigate(MainScreenRoute.Home.route) {
+                        popUpTo(MainScreenRoute.AlarmList.route) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
         }
 
         composable(route = MainScreenRoute.MyPage.route) {
--- app/src/main/java/com/asap/aljyo/ui/composable/main/alarm_list/AlarmListScreen.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/alarm_list/AlarmListScreen.kt
@@ -1,9 +1,32 @@
 package com.asap.aljyo.ui.composable.main.alarm_list
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.asap.aljyo.ui.theme.AljyoTheme
 
 @Composable
-fun AlarmListScreen() {
-    Text("Alarm List")
+internal fun AlarmListScreen(
+    navigateToHome: () -> Unit = {},
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        EmptyAlarmList(
+            modifier = Modifier.fillMaxWidth().align(Alignment.Center),
+            navigateToHome = navigateToHome
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    AljyoTheme {
+        AlarmListScreen()
+    }
 }

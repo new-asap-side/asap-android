8d61a35d0207b44d971b250491b16be3804096eb
dev-JongH <dev.whdgus090@gmail.com>
2024-12-05 17:26:09 +0900

    Test: add DateTimeManager sortByDay test code [ASAP-92]

--- data/src/test/java/com/asap/data/utility/DateTimeTest.kt
+++ data/src/test/java/com/asap/data/utility/DateTimeTest.kt
@@ -11,14 +11,14 @@ class UtilityTest {
         val outputDate1 = DateTimeManager.parseToDay(output1)
 
         assert(output1 == 1260L)
-        assert(outputDate1 == "0일 21시간 0분")
+        assert(outputDate1 == "21시간 00분")
 
         val nextDay2 = "화 20:25"
         val output2 = DateTimeManager.diffFromNow(nextDay2, isTest = true)
         val outputDate2 = DateTimeManager.parseToDay(output2)
 
         assert(output2 == 1915L)
-        assert(outputDate2 == "1일 7시간 55분")
+        assert(outputDate2 == "1일 07시간 55분")
 
         val nextDay3 = "일 11:22"
         val output3 = DateTimeManager.diffFromNow(nextDay3, isTest = true)
@@ -32,21 +32,38 @@ class UtilityTest {
         val outputDate4 = DateTimeManager.parseToDay(output4)
 
         assert(output4 == 8965L)
-        assert(outputDate4 == "6일 5시간 25분")
+        assert(outputDate4 == "6일 05시간 25분")
 
 
         val nextDay5 = "월 11:30"
         val output5 = DateTimeManager.diffFromNow(nextDay5, isTest = true)
         val outputDate5 = DateTimeManager.parseToDay(output5)
 
-        assert(output5 == -60L)
-        assert(outputDate5 == "6일 23시간 0분")
+        assert(output5 == 10020L)
+        assert(outputDate5 == "6일 23시간 00분")
 
         val nextDay6 = "월 17:45"
         val output6 = DateTimeManager.diffFromNow(nextDay6, isTest = true)
         val outputDate6 = DateTimeManager.parseToDay(output6)
 
         assert(output6 == 315L)
-        assert(outputDate6 == "0일 5시간 15분")
+        assert(outputDate6 == "05시간 15분")
+    }
+
+    @Test
+    fun test_DateTimeManager_sortByDay() {
+        // 테스트 시 기준 요일 월요일로 고정
+        val days = listOf(
+            "일 11:30",
+            "수 17:30",
+            "수 12:30",
+            "목 11:30",
+            "월 11:30",
+            "화 11:30",
+            "토 11:30",
+        )
+
+        val sortedDays = DateTimeManager.sortByDay(days, isTest = true)
+        assert(sortedDays[0] == "월 11:30")
     }
 }

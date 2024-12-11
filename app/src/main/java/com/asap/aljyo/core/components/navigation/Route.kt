aa2d143e7bc47402d252dc0dfc072f6ecc3d3f1f
dev-JongH <dev.whdgus090@gmail.com>
2024-12-11 15:17:17 +0900

    Fix: Route name lower case [ASAP-7]

--- app/src/main/java/com/asap/aljyo/core/components/navigation/Route.kt
+++ app/src/main/java/com/asap/aljyo/core/components/navigation/Route.kt
@@ -4,28 +4,28 @@ package com.asap.aljyo.core.components.navigation
 // Application screen
 sealed class ScreenRoute(val route: String) {
 
-    data object Onboarding : ScreenRoute(route = "ON_BOARDING")
+    data object Onboarding : ScreenRoute(route = "on-boarding")
 
-    data object Main : ScreenRoute(route = "MAIN")
+    data object Main : ScreenRoute(route = "main")
 
-    data object GroupDetails : ScreenRoute(route = "GROUP_DETAILS")
+    data object GroupDetails : ScreenRoute(route = "group-details")
 
-    data object Ranking : ScreenRoute(route = "RANKING")
+    data object Ranking : ScreenRoute(route = "ranking")
 
-    data object ReleaseAlarm : ScreenRoute(route = "RELEASE_ALARM")
+    data object ReleaseAlarm : ScreenRoute(route = "release-alarm")
 
-    data object AlarmResult : ScreenRoute(route = "ALARM_RESULT")
+    data object AlarmResult : ScreenRoute(route = "alarm-result")
 
-    data object UserSetting: ScreenRoute(route = "USER_SETTING_PROFILE")
+    data object UserSetting: ScreenRoute(route = "user-setting-profile")
 }
 
 // Main screen route
 sealed class MainScreenRoute(val route: String) {
 
-    data object Home : MainScreenRoute(route = "HOME")
+    data object Home : MainScreenRoute(route = "home")
 
-    data object AlarmList : MainScreenRoute(route = "ALARM_LIST")
+    data object AlarmList : MainScreenRoute(route = "alarm-list")
 
-    data object MyPage : MainScreenRoute(route = "MY_PAGE")
+    data object MyPage : MainScreenRoute(route = "my-page")
 
 }

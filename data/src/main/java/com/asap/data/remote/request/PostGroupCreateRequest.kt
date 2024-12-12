fac63a9145837b9d2060a8587a863e1166202c89
KJ-Hyeon <bark052526@gmail.com>
2024-12-12 13:27:43 +0900

    Feat:그룹 생성 API 서버 요청/응답 형식 데이터 클래스 추가 [ASAP-4]

---
+++ data/src/main/java/com/asap/data/remote/request/PostGroupCreateRequest.kt
@@ -0,0 +1,38 @@
+package com.asap.data.remote.request
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PostGroupCreateRequest(
+    @Json(name = "alarm_day")
+    val alarmDay: String,
+    @Json(name = "alarm_end_date")
+    val alarmEndDate: String,
+    @Json(name = "alarm_time")
+    val alarmTime: String,
+    @Json(name = "alarm_type")
+    val alarmType: String,
+    @Json(name = "alarm_unlock_contents")
+    val alarmUnlockContents: String,
+    @Json(name = "alarm_volume")
+    val alarmVolume: Int,
+    @Json(name = "description")
+    val description: String,
+    @Json(name = "device_token")
+    val deviceToken: String,
+    @Json(name = "device_type")
+    val deviceType: String,
+    @Json(name = "group_password")
+    val groupPassword: String,
+    @Json(name = "is_public")
+    val isPublic: Boolean,
+    @Json(name = "max_person")
+    val maxPerson: Int,
+    @Json(name = "music_title")
+    val musicTitle: String,
+    @Json(name = "title")
+    val title: String,
+    @Json(name = "user_id")
+    val userId: Int
+)

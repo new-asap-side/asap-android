b5bf15dd24fe83614e382f14e80475bd5235e8c4
KJ-Hyeon <bark052526@gmail.com>
2024-12-12 14:43:09 +0900

    Feat:그룹 생성 데이터 소스 메서드 추가 [ASAP-4]

--- data/src/main/java/com/asap/data/remote/datasource/GroupRemoteDataSource.kt
+++ data/src/main/java/com/asap/data/remote/datasource/GroupRemoteDataSource.kt
@@ -1,11 +1,15 @@
 package com.asap.data.remote.datasource
 
+import com.asap.data.remote.request.PostGroupCreateRequest
+import com.asap.data.remote.response.PostGroupCreateResponse
 import com.asap.data.remote.service.GroupService
 import com.asap.domain.entity.remote.AlarmGroup
 import com.asap.domain.entity.remote.GroupRanking
+import com.squareup.moshi.Json
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
+import kotlin.math.max
 
 class GroupRemoteDataSource @Inject constructor(
     private val groupService: GroupService
@@ -24,4 +28,40 @@ class GroupRemoteDataSource @Inject constructor(
         val response = groupService.fetchGroupRanking(groupId = groupId)
         emit(response.body())
     }
+
+    suspend fun postCreateGroup(
+        alarmDay: String,
+        alarmEndDate: String,
+        alarmTime: String,
+        alarmType: String,
+        alarmUnlockContents: String,
+        alarmVolume: Int,
+        description: String,
+        deviceType: String,
+        groupPassword: String,
+        isPublic: Boolean,
+        maxPerson: Int,
+        musicTitle: String,
+        title: String,
+        userId: Int
+    ): PostGroupCreateResponse? {
+        val groupCreateRequest = PostGroupCreateRequest(
+            alarmDay = alarmDay,
+            alarmEndDate = alarmEndDate,
+            alarmTime = alarmTime,
+            alarmType = alarmType,
+            alarmUnlockContents = alarmUnlockContents,
+            alarmVolume = alarmVolume,
+            description = description,
+            deviceType, deviceType,
+            groupPassword = groupPassword,
+            isPublic = isPublic,
+            maxPerson = maxPerson,
+            musicTitle = musicTitle,
+            title = title,
+            userId
+        )
+        return groupService.postCreateGroup(groupCreateRequest).body()
+    }
+
 }

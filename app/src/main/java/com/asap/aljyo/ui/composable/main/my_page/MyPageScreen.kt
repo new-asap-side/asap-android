0f65c36e53f4cd74afdc90f0ecd8cb692fab83a5
dev-JongH <dev.whdgus090@gmail.com>
2024-12-12 17:40:19 +0900

    Feat: add MyPageScreen title [ASAP-7]

--- app/src/main/java/com/asap/aljyo/ui/composable/main/my_page/MyPageScreen.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/my_page/MyPageScreen.kt
@@ -1,9 +1,41 @@
 package com.asap.aljyo.ui.composable.main.my_page
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.asap.aljyo.R
+import com.asap.aljyo.ui.theme.Black01
+import com.asap.aljyo.ui.theme.Grey01
+import com.asap.aljyo.ui.theme.White
 
 @Composable
 fun MyPageScreen() {
-    Text("My Page")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Grey01)
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(White)
+                .padding(vertical = 10.dp),
+            text = stringResource(R.string.my_page),
+            style = MaterialTheme.typography.titleMedium.copy(
+                color = Black01,
+                fontSize = 16.sp
+            ),
+            textAlign = TextAlign.Center
+        )
+    }
 }

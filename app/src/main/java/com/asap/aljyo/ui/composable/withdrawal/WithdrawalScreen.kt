350d04115c09d82d5d96b3b2c375b4132695e67f
dev-JongH <dev.whdgus090@gmail.com>
2024-12-13 16:47:38 +0900

    Feat: add Survey area to WithdrawalScreen [ASAP-101]

--- app/src/main/java/com/asap/aljyo/ui/composable/withdrawal/WithdrawalScreen.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/withdrawal/WithdrawalScreen.kt
@@ -1,9 +1,13 @@
 package com.asap.aljyo.ui.composable.withdrawal
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -16,16 +20,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.asap.aljyo.R
 import com.asap.aljyo.ui.theme.AljyoTheme
 import com.asap.aljyo.ui.theme.Black01
+import com.asap.aljyo.ui.theme.Grey01
 import com.asap.aljyo.ui.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -33,62 +40,74 @@ import com.asap.aljyo.ui.theme.White
 internal fun WithdrawalScreen(
     onBackIconPressed: () -> Unit
 ) {
-    Scaffold(
-        containerColor = White,
-        topBar = {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = White
-                ),
-                title = {
-                    Text(
-                        text = stringResource(R.string.membership_withdrawal),
-                        style = MaterialTheme.typography.titleMedium.copy(
-                            color = Black01,
-                            fontSize = 16.sp
-                        ),
-                    )
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = { onBackIconPressed() }
-                    ) {
-                        Icon(
-                            Icons.Default.Close,
-                            contentDescription = "Close icon"
+    AljyoTheme {
+        Scaffold(
+            containerColor = White,
+            topBar = {
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = White
+                    ),
+                    title = {
+                        Text(
+                            text = stringResource(R.string.membership_withdrawal),
+                            style = MaterialTheme.typography.titleMedium.copy(
+                                color = Black01,
+                                fontSize = 16.sp
+                            ),
                         )
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = { onBackIconPressed() }
+                        ) {
+                            Icon(
+                                Icons.Default.Close,
+                                contentDescription = "Close icon"
+                            )
+                        }
                     }
-                }
-            )
-        }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize()
-        ) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = buildAnnotatedString {
-                    append("${stringResource(R.string.really)} ")
-                    withStyle(
-                        style = SpanStyle(
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                    ) {
-                        append(stringResource(R.string.app_name))
-                    }
-                    append("${stringResource(R.string.from)}\n")
-                    append(stringResource(R.string.are_you_leave))
-                },
-                style = MaterialTheme.typography.headlineMedium.copy(
-                    color = Black01,
-                    fontSize = 20.sp,
-                    lineHeight = 28.sp
-                ),
-                textAlign = TextAlign.Center
-            )
+                )
+            }
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize()
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = buildAnnotatedString {
+                        append("${stringResource(R.string.really)} ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        ) {
+                            append(stringResource(R.string.app_name))
+                        }
+                        append("${stringResource(R.string.from)}\n")
+                        append(stringResource(R.string.are_you_leave))
+                    },
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        color = Black01,
+                        fontSize = 20.sp,
+                        lineHeight = 28.sp
+                    ),
+                    textAlign = TextAlign.Center
+                )
 
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Survey(
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Grey01)
+                )
+
+            }
         }
     }
 }

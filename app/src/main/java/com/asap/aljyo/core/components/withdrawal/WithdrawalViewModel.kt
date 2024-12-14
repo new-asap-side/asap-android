f11db55c1c39aa2968045c394f4cccd9856d61bb
dev-JongH <dev.whdgus090@gmail.com>
2024-12-14 18:08:53 +0900

    Feat: add PrecautionsButton [ASAP-101]

--- app/src/main/java/com/asap/aljyo/core/components/withdrawal/WithdrawalViewModel.kt
+++ app/src/main/java/com/asap/aljyo/core/components/withdrawal/WithdrawalViewModel.kt
@@ -10,7 +10,14 @@ class WithdrawalViewModel @Inject constructor() : ViewModel() {
     private val _selectedIndex = mutableStateOf<Int?>(null)
     val selectedIndex get() = _selectedIndex.value
 
+    private val _checkedPrecautions = mutableStateOf(false)
+    val checkedPrecautions get() = _checkedPrecautions.value
+
     fun select(index: Int?) {
         _selectedIndex.value = index
+    }
+
+    fun checkPrecautions() {
+        _checkedPrecautions.value = !_checkedPrecautions.value
     }
 }

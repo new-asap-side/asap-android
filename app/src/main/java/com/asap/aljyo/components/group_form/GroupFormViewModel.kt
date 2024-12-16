1dfdf57240c542c13701d7e6fd436b2bd8a68aaf
KJ-Hyeon <bark052526@gmail.com>
2024-12-16 22:13:41 +0900

    Feat: savedStateHandle을 이용해 전달받은 Navigation Arguments를 ViewModel에서 관리하도록 구현 [ASAP-4]

--- app/src/main/java/com/asap/aljyo/components/group_form/GroupFormViewModel.kt
+++ app/src/main/java/com/asap/aljyo/components/group_form/GroupFormViewModel.kt
@@ -1,6 +1,7 @@
 package com.asap.aljyo.components.group_form
 
 import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.asap.domain.usecase.group.CreateGroupUseCase
@@ -14,11 +15,19 @@ import javax.inject.Inject
 @HiltViewModel
 class GroupFormViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
-    private val createGroupUseCase: CreateGroupUseCase
 ): ViewModel() {
     private val _groupScreenState = MutableStateFlow(GroupScreenState())
     val groupScreenState: StateFlow<GroupScreenState> get() = _groupScreenState.asStateFlow()
 
     private val _alarmScreenState = MutableStateFlow(AlarmScreenState())
     val alarmScreenState: StateFlow<AlarmScreenState> get() = _alarmScreenState.asStateFlow()
+
+    private val groupType: Boolean? get() = savedStateHandle["groupType"]
+    private val groupPassword: String? get() = savedStateHandle["groupPassword"]
+    private val alarmType: String? get() = savedStateHandle["alarmType"]
+
+    fun check() {
+        Log.d("savedStateHandleCheck","groupType: $groupType groupPassword: $groupPassword, alarmType: $alarmType")
+    }
+
 }

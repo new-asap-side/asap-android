c0120aef4c97bca35c65645b94c23aaac15f49d7
dev-JongH <dev.whdgus090@gmail.com>
2024-12-18 17:38:37 +0900

    Feat: PopularityScreen Loading / Error state 관리 로직 추가  [ASAP-51-android]

--- app/src/main/java/com/asap/aljyo/ui/composable/main/home/popularity/PopularityScreen.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/home/popularity/PopularityScreen.kt
@@ -1,17 +1,24 @@
 package com.asap.aljyo.ui.composable.main.home.popularity
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.asap.aljyo.core.components.main.HomeViewModel
+import com.asap.aljyo.ui.UiState
+import com.asap.aljyo.ui.composable.common.ErrorBox
 import com.asap.aljyo.ui.composable.main.home.GroupItem
+import com.asap.aljyo.ui.composable.main.home.GroupItemShimmer
 
 @Composable
 fun PopularityScreen(
@@ -24,6 +31,8 @@ fun PopularityScreen(
         initialFirstVisibleItemScrollOffset = scrollInfo.second
     )
 
+    val popularityGroupState by viewModel.popularGroupState.collectAsState()
+
     DisposableEffect(scrollState) {
         onDispose {
             val index = scrollState.firstVisibleItemIndex
@@ -35,6 +44,13 @@ fun PopularityScreen(
         }
     }
 
+    if(popularityGroupState is UiState.Error) {
+        ErrorBox(modifier = Modifier.fillMaxSize()) {
+            viewModel.fetchHomeData()
+        }
+        return
+    }
+
     LazyVerticalGrid(
         state = scrollState,
         modifier = Modifier.padding(horizontal = 20.dp),
@@ -42,8 +58,30 @@ fun PopularityScreen(
         verticalArrangement = Arrangement.spacedBy(24.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items(20) {
-            GroupItem()
+        when (popularityGroupState) {
+            UiState.Loading -> {
+                repeat(6) {
+                    item {
+                        GroupItemShimmer(modifier = Modifier)
+                    }
+                }
+            }
+
+            is UiState.Success -> {
+                val latestGroup = (popularityGroupState as UiState.Success).data ?: emptyList()
+                latestGroup.forEach { group ->
+                    item {
+                        GroupItem(
+                            modifier = Modifier.clickable {
+                                navigateToGroupDetails(group.groupId)
+                            },
+                            alarmGroup = group,
+                        )
+                    }
+                }
+            }
+
+            else -> Unit
         }
     }
 }

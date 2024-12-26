ca3d17efcd43f7158acb5b1d6b03a4c73d439e97
dev-JongH <dev.whdgus090@gmail.com>
2024-12-26 16:56:12 +0900

    Fix: GroupBottomNavBar height [ASAP-149-android]

--- app/src/main/java/com/asap/aljyo/ui/composable/group_details/GroupBottomNavBar.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/group_details/GroupBottomNavBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -58,19 +59,21 @@ internal fun GroupBottomNavBar(
         modifier = modifier,
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
+        val mod = Modifier.weight(1f).height(52.dp)
+
         when (userGroupType) {
             UserGroupType.NonParticipant -> NonParticipantBottomBar(
-                modifier = Modifier.weight(1f)
+                modifier = mod
             )
 
             UserGroupType.Leader -> ParticipantBottomBar(
-                modifier = Modifier.weight(1f),
+                modifier = mod,
                 isLeader = true,
                 onRankingClick = onRankingClick
             )
 
             UserGroupType.Participant -> ParticipantBottomBar(
-                modifier = Modifier.weight(1f),
+                modifier = mod,
                 isLeader = false,
                 onRankingClick = onRankingClick
             )

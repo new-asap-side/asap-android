703615218513eaa64cc989bffe2a4f52c13e87e1
KJ-Hyeon <bark052526@gmail.com>
2025-01-04 01:37:17 +0900

    Feat:그룹 수정 요청 모델 추가[ASAP-16]

---
+++ data/src/main/java/com/asap/data/remote/request/PostGroupEditRequest.kt
@@ -0,0 +1,22 @@
+package com.asap.data.remote.request
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PostGroupEditRequest(
+    @Json(name = "user_id")
+    val userId: Int,
+    @Json(name = "group_id")
+    val groupId: Int,
+    @Json(name = "title")
+    val title: String,
+    @Json(name = "description")
+    val description: String,
+    @Json(name = "max_person")
+    val maxPerson: Int,
+    @Json(name = "alarm_unlock_contents")
+    val alarmUnlockContents: String,
+    @Json(name = "is_public")
+    val isPublic: Boolean
+)

8bf199aceacad21486e0e5bf0c948c2ee1944c60
KJ-Hyeon <bark052526@gmail.com>
2025-01-04 01:41:22 +0900

    Feat:그룹 수정 UseCase 추가[ASAP-16]

---
+++ domain/src/main/java/com/asap/domain/usecase/group/EditGroupUseCase.kt
@@ -0,0 +1,31 @@
+package com.asap.domain.usecase.group
+
+import com.asap.domain.repository.GroupRepository
+import com.asap.domain.repository.UserRepository
+import javax.inject.Inject
+
+class EditGroupUseCase @Inject constructor(
+    private val groupRepository: GroupRepository,
+    private val userRepository: UserRepository
+) {
+    suspend operator fun invoke(
+        groupId: Int,
+        title: String,
+        description: String,
+        maxPerson: Int,
+        alarmUnlockContents: String,
+        isPublic: Boolean
+    ) {
+        val userId = userRepository.getUserInfo().userId.toInt()
+
+        groupRepository.postGroupEdit(
+            userId = userId,
+            groupId = groupId,
+            title = title,
+            description = description,
+            maxPerson = maxPerson,
+            alarmUnlockContents = alarmUnlockContents,
+            isPublic = isPublic
+        )
+    }
+}

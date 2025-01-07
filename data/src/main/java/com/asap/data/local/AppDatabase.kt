a7dcfc3051c7b6c7903a3b445cc2bde15c3ca4c9
dev-JongH <dev.whdgus090@gmail.com>
2025-01-07 11:13:35 +0900

    Feat: add DeactivatedAlarmDao [ASAP-159-android]

--- data/src/main/java/com/asap/data/local/AppDatabase.kt
+++ data/src/main/java/com/asap/data/local/AppDatabase.kt
@@ -2,10 +2,18 @@ package com.asap.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.asap.data.local.dao.DeactivatedAlarmDao
 import com.asap.data.local.dao.UserDao
+import com.asap.domain.entity.local.DeactivatedAlarm
 import com.asap.domain.entity.local.User
 
-@Database(entities = [User::class], version = 2, exportSchema = false)
+@Database(
+    entities = [User::class, DeactivatedAlarm::class],
+    version = 2,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
+
+    abstract fun deactivatedAlarmDao(): DeactivatedAlarmDao
 }
---
+++ data/src/main/java/com/asap/data/local/dao/DeactivatedAlarmDao.kt
@@ -0,0 +1,19 @@
+package com.asap.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.asap.domain.entity.local.DeactivatedAlarm
+
+@Dao
+interface DeactivatedAlarmDao {
+    @Query("SELECT * FROM DeactivatedAlarm WHERE group_id = :groupId")
+    suspend fun find(groupId: Int): DeactivatedAlarm?
+
+    @Insert
+    suspend fun insert(deactivatedAlarm: DeactivatedAlarm)
+
+    @Delete
+    suspend fun delete(deactivatedAlarm: DeactivatedAlarm)
+}

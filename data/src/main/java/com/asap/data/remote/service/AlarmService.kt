285427a09f69a6f8aa4f4cb3004f22f73de3b571
dev-JongH <dev.whdgus090@gmail.com>
2025-01-08 11:02:58 +0900

    Feat: add AlarmRemoteDataSource [ASAP-159-android]

--- data/src/main/java/com/asap/data/remote/service/AlarmService.kt
+++ data/src/main/java/com/asap/data/remote/service/AlarmService.kt
@@ -1,5 +1,6 @@
 package com.asap.data.remote.service
 
+import com.asap.domain.entity.remote.AlarmOffRequestBody
 import com.asap.domain.entity.remote.WhetherResponse
 import retrofit2.Response
 import retrofit2.http.Body
@@ -8,5 +9,5 @@ import retrofit2.http.POST
 interface AlarmService {
     // 알람 해제
     @POST("/alarm/off")
-    suspend fun release(@Body boy: Map<String, String>): Response<WhetherResponse?>
+    suspend fun release(@Body boy: AlarmOffRequestBody): Response<WhetherResponse?>
 }

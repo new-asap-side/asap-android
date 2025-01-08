c391977628d043c414197579afcd34c518c75a0b
dev-JongH <dev.whdgus090@gmail.com>
2025-01-08 14:46:03 +0900

    Feat: add FetchRankingNumberUseCase [ASAP-159-android]

--- domain/src/main/java/com/asap/domain/usecase/group/GroupRankingUseCase.kt
+++ domain/src/main/java/com/asap/domain/usecase/group/GroupRankingUseCase.kt
@@ -1,6 +1,7 @@
 package com.asap.domain.usecase.group
 
 import com.asap.domain.entity.remote.GroupRanking
+import com.asap.domain.entity.remote.RankingNumberResponse
 import com.asap.domain.repository.GroupRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -14,5 +15,18 @@ class FetchGroupRankingUseCaseImpl @Inject constructor(
 ) : FetchGroupRankingUseCase {
     override suspend fun invoke(groupId: Int): Flow<List<GroupRanking>?> =
         groupRepository.fetchGroupRanking(groupId = groupId)
+
+}
+
+interface FetchRankingNumberUseCase {
+    suspend operator fun invoke(groupId: Int): Flow<RankingNumberResponse?>
+}
+
+class FetchRankingNumberUseCaseImpl @Inject constructor(
+    private val groupRepository: GroupRepository
+) : FetchRankingNumberUseCase {
+    override suspend fun invoke(groupId: Int): Flow<RankingNumberResponse?> {
+        TODO("Not yet implemented")
+    }
 
 }

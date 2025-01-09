5074c6c69eb5a1dab8d964144f47bdbbe615a4a9
dev-JongH <dev.whdgus090@gmail.com>
2025-01-09 11:17:46 +0900

    Feat: add FetchUserProfileUseCase [ASAP-159-android]

---
+++ domain/src/main/java/com/asap/domain/usecase/group/FetchUserProfileUseCase.kt
@@ -0,0 +1,19 @@
+package com.asap.domain.usecase.group
+
+import com.asap.domain.entity.remote.user.UserProfile
+import com.asap.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface FetchUserProfileUseCase {
+    suspend operator fun invoke(): Flow<UserProfile?>
+}
+
+class FetchUserProfileUseCaseImpl @Inject constructor(
+    private val userRepository: UserRepository
+) : FetchUserProfileUseCase {
+    override suspend fun invoke(): Flow<UserProfile?> {
+        return userRepository.fetchUserProfile()
+    }
+
+}

e84de4178fe28084a714230bca2a2476e26d2db5
dev-JongH <dev.whdgus090@gmail.com>
2025-01-10 10:21:34 +0900

    Refactor: usecase로 registerToken 호출 [ASAP-159-android]

--- app/src/main/java/com/asap/aljyo/core/components/Application.kt
+++ app/src/main/java/com/asap/aljyo/core/components/Application.kt
@@ -1,14 +1,9 @@
 package com.asap.aljyo.core.components
 
 import android.app.Application
-import android.util.Log
 import com.asap.aljyo.BuildConfig
 import com.asap.aljyo.util.PictureUtil
-import com.asap.data.remote.firebase.FCMTokenManager
-import com.asap.data.repository.AuthRepositoryImpl.Companion.TAG
-import com.asap.domain.repository.AuthRepository
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
+import com.asap.domain.usecase.auth.RegisterTokenUseCase
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
@@ -19,7 +14,7 @@ import javax.inject.Inject
 @HiltAndroidApp
 class AljyoApplication : Application() {
     @Inject
-    lateinit var authRepository: AuthRepository
+    lateinit var registerTokenUseCase: RegisterTokenUseCase
 
     override fun onCreate() {
         super.onCreate()
@@ -30,7 +25,7 @@ class AljyoApplication : Application() {
         PictureUtil.setContext(this)
 
         CoroutineScope(Dispatchers.Default).launch {
-            authRepository.registerToken()
+            registerTokenUseCase()
         }
     }
 }

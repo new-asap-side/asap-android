9bcc2d4266680cbf0d9ac3311eaea37c53452896
dev-JongH <dev.whdgus090@gmail.com>
2025-01-18 16:03:17 +0900

    Fix: 토큰 갱신 로직

--- data/src/main/java/com/asap/data/remote/Authenticator.kt
+++ data/src/main/java/com/asap/data/remote/Authenticator.kt
@@ -3,9 +3,6 @@ package com.asap.data.remote
 import android.util.Log
 import com.asap.data.local.source.SessionLocalDataSource
 import com.asap.domain.usecase.auth.RefreshTokenUseCase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import okhttp3.Authenticator
 import okhttp3.Request
@@ -22,15 +19,14 @@ class TokenAuthenticator @Inject constructor(
         Log.i(tag, "Access token is expired ...")
 
         return try {
-            val refreshToken = runBlocking { sessionLocalDataSource.getRefreshToken() }
-            Log.i(tag, "Retry with refresh token: $refreshToken")
-
-            CoroutineScope(Dispatchers.IO).launch {
+            val accessToken = runBlocking {
                 refreshTokenUseCase()
-            }
+                sessionLocalDataSource.getAccessToken()
+            } ?: ""
+            Log.i(tag, "Refresh access token: $accessToken")
 
             response.request.newBuilder().removeHeader(HeaderInterceptor.AUTH_KEY).apply {
-                addHeader(HeaderInterceptor.AUTH_KEY, "Bearer $refreshToken")
+                addHeader(HeaderInterceptor.AUTH_KEY, "Bearer $accessToken")
             }.build()
         } catch (e: Exception) {
             Log.e(tag, "$e")

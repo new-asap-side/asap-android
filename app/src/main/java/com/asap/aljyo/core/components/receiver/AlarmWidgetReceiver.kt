55ef7efb84e0d3fa1caaecb5535b922844d8f970
dev-JongH <dev.whdgus090@gmail.com>
2025-01-20 15:59:30 +0900

    Feat: add AlarmWidgetReceiver

---
+++ app/src/main/java/com/asap/aljyo/core/components/receiver/AlarmWidgetReceiver.kt
@@ -0,0 +1,9 @@
+package com.asap.aljyo.core.components.receiver
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class AlarmWidgetReceiver : GlanceAppWidgetReceiver() {
+
+    override val glanceAppWidget: GlanceAppWidget = TODO("Not Implements")
+}

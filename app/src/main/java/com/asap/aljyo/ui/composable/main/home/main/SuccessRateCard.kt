e78ce0fa2c5bb8e3b1be5809ba4f968db262fb25
dev-JongH <dev.whdgus090@gmail.com>
2025-02-27 22:57:27 +0900

    Feat: SuccessRateCard 상단 UI

--- app/src/main/java/com/asap/aljyo/ui/composable/main/home/main/SuccessRateCard.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/home/main/SuccessRateCard.kt
@@ -2,7 +2,6 @@ package com.asap.aljyo.ui.composable.main.home.main
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,8 +9,12 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -21,14 +24,23 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.asap.aljyo.R
 import com.asap.aljyo.core.components.viewmodel.main.AlarmSuccessRateViewModel
+import com.asap.aljyo.core.fsp
 import com.asap.aljyo.ui.UiState
 import com.asap.aljyo.ui.composable.common.extension.dropShadow
 import com.asap.aljyo.ui.composable.common.loading.ShimmerBox
 import com.asap.aljyo.ui.theme.AljyoTheme
+import com.asap.aljyo.ui.theme.Black01
+import com.asap.aljyo.ui.theme.White
 
 private val stops = arrayOf(
     0.28f to Color(0xFFFFEEF3),
@@ -40,17 +52,19 @@ fun SuccessRateCard(
     modifier: Modifier = Modifier
 ) {
     val viewModel: AlarmSuccessRateViewModel = hiltViewModel()
-    val successRate by viewModel.successRateState.collectAsState()
+    val successRateState by viewModel.successRateState.collectAsState()
+    val user by viewModel.user.collectAsState()
 
-    when (successRate) {
+    when (successRateState) {
         is UiState.Error -> Unit
         UiState.Loading -> SuccessRateCardShimmer(modifier = modifier)
         is UiState.Success -> {
+            val successRate = (successRateState as UiState.Success).data
             Column(
                 modifier = modifier,
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                Box(
+                Column(
                     modifier = Modifier
                         .fillMaxWidth()
                         .aspectRatio(320f / 254f)
@@ -67,9 +81,16 @@ fun SuccessRateCard(
                                 start = Offset(0f, 0f),
                                 end = Offset(Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY),
                             )
-                        ),
+                        )
+                        .padding(vertical = 28.dp, horizontal = 24.dp),
                 ) {
+                    SuccessRate(
+                        modifier = Modifier.fillMaxWidth(),
+                        nickname = user?.nickname ?: "",
+                        rate = successRate?.offRate ?: 0f
+                    )
 
+                    SuccessRateProgress()
                 }
 
                 Row(
@@ -97,6 +118,79 @@ fun SuccessRateCard(
     }
 }
 
+@Composable
+private fun SuccessRate(
+    modifier: Modifier = Modifier,
+    nickname: String,
+    rate: Float
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFF111111)
+                        )
+                    ) { append(nickname) }
+                    append(stringResource(R.string.your_alarm_off_success_rate))
+                },
+                style = MaterialTheme.typography.labelMedium.copy(
+                    fontSize = 15.fsp,
+                    color = Black01
+                )
+            )
+
+            Text(
+                text = buildAnnotatedString {
+                    append(stringResource(R.string.this_month))
+
+                    withStyle(
+                        style = SpanStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 26.fsp,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    ) { append(" ${stringResource(R.string.rate, rate)} ") }
+
+                    append(stringResource(R.string.success))
+                },
+                style = MaterialTheme.typography.titleMedium.copy(
+                    fontSize = 23.fsp,
+                )
+            )
+        }
+
+        TextButton(
+            onClick = {},
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = White,
+                contentColor = Color(0xFF666666)
+            )
+        ) {
+            Text(
+                text = stringResource(R.string.use_information),
+                style = MaterialTheme.typography.titleMedium.copy(
+                    fontSize = 13.fsp,
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun SuccessRateProgress() {
+
+}
+
+// preview
 @Composable
 private fun SuccessRateCardShimmer(modifier: Modifier) {
     val shape = RoundedCornerShape(4.dp)

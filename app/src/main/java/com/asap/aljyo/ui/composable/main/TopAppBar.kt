d96a204542d4f6d8cb1611e6455a884178a1b04a
dev-Jong <dev.whdgus090@gmail.com>
2025-03-06 18:12:07 +0900

    Feat: SearchBar UI

--- app/src/main/java/com/asap/aljyo/ui/composable/main/TopAppBar.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/TopAppBar.kt
@@ -6,7 +6,9 @@ import android.util.Log
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.core.animate
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,6 +27,7 @@ import androidx.compose.ui.res.stringResource
 import com.asap.aljyo.R
 import com.asap.aljyo.core.components.viewmodel.main.MainViewModel
 import com.asap.aljyo.core.fsp
+import com.asap.aljyo.ui.composable.SharedElement
 import com.asap.aljyo.ui.theme.Black01
 import com.asap.aljyo.ui.theme.White
 
@@ -68,7 +71,7 @@ private fun HomeTopAppBar(
             title = {
                 Image(
                     modifier = Modifier.sharedElement(
-                        rememberSharedContentState("search-title"),
+                        rememberSharedContentState(SharedElement.SEARCH_TITLE),
                         animatedVisibilityScope = animatedContentScope
                     ),
                     painter = painterResource(R.drawable.ic_aljo),
@@ -77,16 +80,27 @@ private fun HomeTopAppBar(
                 )
             },
             actions = {
-                IconButton(
-                    onClick = {
-                        Log.d("TopAppBar", "search icon click !")
-                        navigateToSearch()
-                    }
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_search),
-                        contentDescription = "top bar action[search]"
+                Box(
+                    modifier = Modifier.sharedElement(
+                        rememberSharedContentState(SharedElement.SEARCH_BAR),
+                        animatedVisibilityScope = animatedContentScope
                     )
+                ) {
+                    IconButton(
+                        modifier = Modifier.sharedElement(
+                            rememberSharedContentState(SharedElement.SEARCH_ICON),
+                            animatedVisibilityScope = animatedContentScope
+                        ),
+                        onClick = {
+                            Log.d("TopAppBar", "search icon click !")
+                            navigateToSearch()
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_search),
+                            contentDescription = "top bar action[search]"
+                        )
+                    }
                 }
             },
             colors = TopAppBarDefaults.topAppBarColors(containerColor = White)
--- app/src/main/java/com/asap/aljyo/ui/composable/search/SearchTopBar.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/search/SearchTopBar.kt
@@ -3,15 +3,37 @@ package com.asap.aljyo.ui.composable.search
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import com.asap.aljyo.R
+import com.asap.aljyo.core.fsp
+import com.asap.aljyo.ui.composable.SharedElement
 import com.asap.aljyo.ui.theme.Black00
 import com.asap.aljyo.ui.theme.White
 
@@ -23,27 +45,93 @@ fun SearchTopBar(
     animatedContentScope: AnimatedContentScope,
     onBackClick: () -> Unit,
 ) {
-    with (sharedTransitionScope) {
-        TopAppBar(
-            modifier = modifier,
-            title = {
-                IconButton(
-                    onClick = onBackClick
+    TopAppBar(
+        modifier = modifier,
+        title = {
+            with(sharedTransitionScope) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Icon(
-                        modifier = Modifier.sharedElement(
-                            sharedTransitionScope.rememberSharedContentState(key = "search-title"),
-                            animatedVisibilityScope = animatedContentScope
+                    val source = remember { MutableInteractionSource() }
+                    var target by remember { mutableStateOf("") }
+
+                    IconButton(onClick = onBackClick) {
+                        Icon(
+                            modifier = Modifier.sharedElement(
+                                rememberSharedContentState(SharedElement.SEARCH_TITLE),
+                                animatedVisibilityScope = animatedContentScope
+                            ),
+                            painter = painterResource(R.drawable.ic_top_back),
+                            contentDescription = "back button icon"
+                        )
+                    }
+
+                    BasicTextField(
+                        modifier = Modifier
+                            .sharedElement(
+                                rememberSharedContentState(SharedElement.SEARCH_BAR),
+                                animatedVisibilityScope = animatedContentScope
+                            )
+                            .weight(1f)
+                            .height(44.dp),
+                        value = target,
+                        singleLine = true,
+                        onValueChange = { target = it },
+                        textStyle = MaterialTheme.typography.labelMedium.copy(
+                            fontSize = 15.fsp,
+                            color = Black00
                         ),
-                        painter = painterResource(R.drawable.ic_top_back),
-                        contentDescription = "back button icon"
-                    )
+                    ) { innerTextField ->
+                        TextFieldDefaults.DecorationBox(
+                            value = target,
+                            innerTextField = innerTextField,
+                            enabled = true,
+                            singleLine = true,
+                            interactionSource = source,
+                            visualTransformation = VisualTransformation.None,
+                            contentPadding = PaddingValues(start = 12.dp),
+                            trailingIcon = {
+                                IconButton(
+                                    onClick = {}
+                                ) {
+                                    Icon(
+                                        modifier = Modifier.sharedElement(
+                                            rememberSharedContentState(SharedElement.SEARCH_ICON),
+                                            animatedVisibilityScope = animatedContentScope
+                                        ),
+                                        painter = painterResource(R.drawable.ic_search),
+                                        contentDescription = "search bar tailing icon"
+                                    )
+                                }
+                            },
+                            shape = RoundedCornerShape(8.dp),
+                            placeholder = {
+                                Text(
+                                    text = "알람 제목을 검색해보세요",
+                                    style = MaterialTheme.typography.labelMedium.copy(
+                                        fontSize = 15.fsp,
+                                        color = Color(0xFFAAAAAA)
+                                    ),
+                                )
+                            },
+                            colors = TextFieldDefaults.colors(
+                                focusedContainerColor = Color(0xFFF5F5F5),
+                                unfocusedContainerColor = Color(0xFFF5F5F5),
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                            )
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(16.dp))
                 }
-            },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = White,
-                titleContentColor = Black00
-            )
+            }
+
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = White,
+            titleContentColor = Black00
         )
-    }
+    )
 }

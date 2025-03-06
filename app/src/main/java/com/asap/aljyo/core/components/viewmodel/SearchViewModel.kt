6cd809386737dd7dad8baba35da621923f201187
dev-Jong <dev.whdgus090@gmail.com>
2025-03-06 21:10:38 +0900

    Feat: add SearchViewModel

---
+++ app/src/main/java/com/asap/aljyo/core/components/viewmodel/SearchViewModel.kt
@@ -0,0 +1,8 @@
+package com.asap.aljyo.core.components.viewmodel
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor() : NetworkViewModel() {
+}

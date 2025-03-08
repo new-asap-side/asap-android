6d23fc4e790f193337fb9104bf3b86b6a6696ce1
dev-Jong <dev.whdgus090@gmail.com>
2025-03-08 16:53:40 +0900

    Fix: SearchEntity PrimaryKey 수정

--- domain/src/main/java/com/asap/domain/entity/local/SearchEntity.kt
+++ domain/src/main/java/com/asap/domain/entity/local/SearchEntity.kt
@@ -7,8 +7,6 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "Recent_search_history")
 data class SearchEntity(
     @PrimaryKey
-    @ColumnInfo("id")
-    val id: Int,
     @ColumnInfo("query")
     val query: String,
     @ColumnInfo("date")

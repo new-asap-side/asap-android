e7bc9e030ff00dfd4822b1f5c55e367365c066f2
dev-Jong <dev.whdgus090@gmail.com>
2025-03-10 18:16:21 +0900

    Feat: 밀어서 알람 해제 content 신규 UI 적용

--- app/src/main/java/com/asap/aljyo/ui/composable/alarm_off/Slide.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/alarm_off/Slide.kt
@@ -1,6 +1,7 @@
 package com.asap.aljyo.ui.composable.alarm_off
 
 import android.widget.Toast
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
@@ -10,14 +11,16 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -28,6 +31,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,18 +40,23 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.asap.aljyo.R
 import com.asap.aljyo.core.components.service.AlarmService
 import com.asap.aljyo.core.components.viewmodel.AlarmOffViewModel
 import com.asap.aljyo.core.fsp
 import com.asap.aljyo.ui.RequestState
+import com.asap.aljyo.ui.theme.Red02
 import com.asap.aljyo.ui.theme.White
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.math.max
 
 data class Slide(
@@ -150,61 +159,62 @@ data class Slide(
     @Composable
     override fun Content() {
         val viewModel = provide()
+        val scope = rememberCoroutineScope()
 
         Box(modifier = modifier) {
-            var completed by remember { mutableStateOf(false) }
             var position by remember { mutableFloatStateOf(0f) }
+            var completed by remember { mutableStateOf(false) }
+            var dragVisible by remember { mutableStateOf(true) }
 
-            val minimumOffset = LocalDensity.current.run { 180.dp.toPx() }
+            val threshold = LocalDensity.current.run { 180.dp.toPx() }
             val completeAnimation = animateIntAsState(
-                if (position >= minimumOffset) (position + 200).toInt() else 0,
-                animationSpec = tween(durationMillis = 300),
+                if (completed) (position + 500).toInt() else 0,
+                animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing),
                 label = "drag completed",
                 finishedListener = {
-                    completed = true
                     viewModel.alarmOff(id)
                 }
             )
 
             Image(
-                modifier = Modifier.align(Alignment.Center),
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .layout { measurable, constraints ->
+                        measurable.measure(constraints).run {
+                            layout(width, height) {
+                                place(0, (constraints.maxHeight * 0.2746).toInt())
+                            }
+                        }
+                    }
+                    .width(166.dp),
                 painter = if (completed)
                     painterResource(R.drawable.img_unlock)
                 else
                     painterResource(R.drawable.img_lock),
-                contentDescription = "Lock/Unlock image"
+                contentDescription = "Lock/Unlock image",
+                contentScale = ContentScale.FillWidth
             )
 
-            Card(
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .layout { measurables, constraints ->
-                        val placeable = measurables.measure(constraints)
-                        val xOffset = (placeable.width * 0.1167) + position
-                        val yOffset = (placeable.height * 0.1339).toInt()
-
-                        layout(placeable.width, placeable.height) {
-                            placeable.place(xOffset.toInt() + completeAnimation.value, yOffset)
-                        }
-                    }
-                    .pointerInput(Unit) {
-                        detectDragGestures(
-                            onDragCancel = {
-                                position = 0f
-                            },
-                            onDragEnd = {
-                                if (position >= minimumOffset) {
-                                    return@detectDragGestures
+                        measurables
+                            .measure(constraints)
+                            .run {
+                                layout(width, height) {
+                                    IntOffset(
+                                        (width * 0.1167).toInt(),
+                                        (height * 0.1339).toInt()
+                                    ).let {
+                                        place(it.x + completeAnimation.value, it.y)
+                                    }
                                 }
-                                position = 0f
                             }
-                        ) { _, dragAmount ->
-                            position = max(0f, position + dragAmount.x)
-                        }
                     }
                     .clip(radius)
                     .border(
-                        width = 1.dp,
+                        width = 0.5.dp,
                         shape = radius,
                         brush = Brush.linearGradient(
                             listOf(
@@ -220,11 +230,111 @@ data class Slide(
                             end = Offset(0f, Float.POSITIVE_INFINITY),
                         )
                     ),
-                colors = CardDefaults.cardColors(
-                    containerColor = Color(0xFFFFA9C4).copy(alpha = 0.4f),
-                ),
             ) {
+                if (dragVisible) {
+                    // drag area
+                    Box(
+                        modifier = Modifier
+                            .layout { measurable, constraints ->
+                                measurable
+                                    .measure(
+                                        constraints.copy(
+                                            maxWidth = (constraints.maxWidth * 0.8389).toInt(),
+                                        )
+                                    )
+                                    .run {
+                                        layout(width, height) {
+                                            place(
+                                                (constraints.maxWidth * 0.0806).toInt(),
+                                                (constraints.maxHeight * 0.3543).toInt()
+                                            )
+                                        }
+                                    }
+                            }
+                            .fillMaxWidth()
+                            .height(80.dp)
+                            .background(
+                                shape = RoundedCornerShape(24.dp),
+                                color = Color(0xFFFF73A5)
+                            )
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .padding(12.dp)
+                                .width(90.dp)
+                                .fillMaxHeight()
+                                .layout { measurable, constraints ->
+                                    measurable
+                                        .measure(constraints)
+                                        .run {
+                                            layout(constraints.maxWidth, height) {
+                                                place(position.toInt(), 0)
+                                            }
+                                        }
+                                }
+                                .pointerInput(Unit) {
+                                    detectDragGestures(
+                                        onDragCancel = { position = 0f },
+                                        onDragEnd = {
+                                            if (position >= threshold) {
+                                                repeat(100) { position += 1f }
+                                                scope.launch {
+                                                    dragVisible = false
 
+                                                    delay(300)
+                                                    completed = true
+                                                }
+                                                return@detectDragGestures
+                                            }
+                                            position = 0f
+                                        }
+                                    ) { _, dragAmount ->
+                                        position = max(0f, position + dragAmount.x)
+                                    }
+                                }
+                                .background(
+                                    shape = RoundedCornerShape(12.dp),
+                                    color = Red02
+                                ),
+                        )
+
+                        if (position == 0f) {
+                            Icon(
+                                modifier = Modifier
+                                    .padding(vertical = 12.dp)
+                                    .fillMaxHeight()
+                                    .aspectRatio(81f / 54f)
+                                    .offset(x = 90.dp),
+                                painter = painterResource(R.drawable.ic_direct),
+                                contentDescription = "drag direct",
+                                tint = Color.Unspecified,
+                            )
+                        }
+                    }
+                }
+
+                // finger icon
+                if (position == 0f) {
+                    Icon(
+                        modifier = Modifier
+                            .layout { measurable, constraints ->
+                                measurable
+                                    .measure(constraints)
+                                    .run {
+                                        layout(width, height) {
+                                            place(
+                                                (constraints.maxWidth * 0.1139).toInt(),
+                                                (constraints.maxHeight * 0.4234).toInt()
+                                            )
+                                        }
+                                    }
+                            }
+                            .size(90.dp),
+                        painter = painterResource(R.drawable.ic_hand),
+                        contentDescription = "hand pointing icon",
+                        tint = Color.Unspecified
+                    )
+                }
             }
         }
     }

3b9f3ba020f82b502b5fe22d073c096be68d5b75
KJ-Hyeon <bark052526@gmail.com>
2025-03-11 00:06:51 +0900

    Feat:프로필 아이템 & 랭크 점수 불러오기 api 연동 [ASAP-213]

--- app/src/main/java/com/asap/aljyo/ui/composable/main/my_page/CustomizeProfileScreen.kt
+++ app/src/main/java/com/asap/aljyo/ui/composable/main/my_page/CustomizeProfileScreen.kt
@@ -1,9 +1,6 @@
 package com.asap.aljyo.ui.composable.main.my_page
 
-import android.graphics.Paint.Align
-import android.util.Log
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -19,15 +16,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -44,18 +37,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import coil3.DrawableImage
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil3.compose.AsyncImage
 import com.asap.aljyo.R
+import com.asap.aljyo.core.components.main.CustomizeProfileViewModel
 import com.asap.aljyo.core.fsp
 import com.asap.aljyo.ui.composable.common.CustomButton
 import com.asap.aljyo.ui.theme.AljyoTheme
@@ -74,7 +66,8 @@ data class ProfileCustom(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomizeProfileScreen(
-    onBackClick: () -> Unit,
+    viewModel: CustomizeProfileViewModel = hiltViewModel(),
+    onBackClick: () -> Unit
 ) {
     val dummy = remember {
         mutableStateListOf(

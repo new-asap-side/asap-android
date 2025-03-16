ae649ef1c5628276d32489315e0a991008adecc1
dev-Jong <dev.whdgus090@gmail.com>
2025-03-16 16:36:09 +0900

    Refactor: Ranking 리스트 관리 코드 개선

--- app/src/main/java/com/asap/aljyo/core/components/group_ranking/GroupRankingViewModel.kt
+++ app/src/main/java/com/asap/aljyo/core/components/group_ranking/GroupRankingViewModel.kt
@@ -1,10 +1,11 @@
 package com.asap.aljyo.core.components.group_ranking
 
-import android.util.Log
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.asap.aljyo.core.components.viewmodel.NetworkViewModel
 import com.asap.aljyo.ui.UiState
 import com.asap.domain.entity.remote.GroupRanking
 import com.asap.domain.usecase.group.FetchGroupRankingUseCase
@@ -12,19 +13,25 @@ import com.asap.domain.usecase.user.GetUserInfoUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
+
+@Stable
+data class RankingScreenState(
+    val selectedTabIndex: Int,
+    val ranks: List<GroupRanking> = emptyList()
+)
 
 class GroupRankingViewModel @AssistedInject constructor(
+    @Assisted private val groupId: Int,
     private val fetchGroupRankingUseCase: FetchGroupRankingUseCase,
     private val getUserInfoUseCase: GetUserInfoUseCase,
-    @Assisted private val groupId: Int
-) : ViewModel() {
-    private val _state = MutableStateFlow<UiState<List<GroupRanking>?>>(UiState.Loading)
+) : NetworkViewModel() {
+    override val prefix: String = "GroupRanking"
+
+    private val _state = MutableStateFlow<UiState<RankingScreenState>>(UiState.Loading)
     val state get() = _state.asStateFlow()
 
     private val _mIndex = mutableStateOf<Int?>(null)
@@ -36,21 +43,18 @@ class GroupRankingViewModel @AssistedInject constructor(
 
     fun fetchGroupRanking() = viewModelScope.launch {
         _state.value = UiState.Loading
-        delay(500)
         fetchGroupRankingUseCase(groupId).catch { e ->
-            Log.e(TAG, "fetchGroupRanking error - $e")
-            val errorCode = when (e) {
-                is HttpException -> e.code()
-                else -> -1
-            }
-            _state.value = UiState.Error(errorCode = errorCode)
-        }.collect { rankingList ->
-            val mNickname = getUserInfoUseCase().let { it?.nickname }
-            _mIndex.value = rankingList?.indexOf(
-                rankingList.find { mNickname == it.nickName }
-            )
+            _state.emit(handleThrowable(e))
+        }.collect { result ->
+            result?.let { ranks ->
+                _mIndex.value = getUserInfoUseCase()?.let { user ->
+                    ranks.indexOf(ranks.find { user.nickname == it.nickName })
+                }
 
-            _state.value = UiState.Success(rankingList)
+                _state.emit(
+                    UiState.Success(RankingScreenState(0, ranks))
+                )
+            }
         }
     }
 
@@ -59,11 +63,8 @@ class GroupRankingViewModel @AssistedInject constructor(
             return emptyList()
         }
 
-        val ranklist = (_state.value as UiState.Success).data ?: emptyList()
-        return if (ranklist.size < 3) {
-            ranklist
-        } else {
-            ranklist.subList(0, 3)
+        return (_state.value as UiState.Success).data.ranks.let { ranks ->
+            if (ranks.size < 3) ranks else ranks.subList(0, 3)
         }
     }
 
@@ -72,11 +73,8 @@ class GroupRankingViewModel @AssistedInject constructor(
             return emptyList()
         }
 
-        val ranklist = (_state.value as UiState.Success).data ?: emptyList()
-        return if (ranklist.size < 3) {
-            emptyList()
-        } else {
-            ranklist.subList(3, ranklist.size)
+        return (_state.value as UiState.Success).data.ranks.let { ranks ->
+            if (ranks.size < 3) emptyList() else ranks.drop(3)
         }
     }
 

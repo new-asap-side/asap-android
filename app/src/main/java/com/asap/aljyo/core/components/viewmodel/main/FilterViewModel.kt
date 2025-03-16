b5b4e2bb9c0567a7e93152897a73a24959d2d5d0
dev-Jong <dev.whdgus090@gmail.com>
2025-03-16 12:48:51 +0900

    Feat: add FilterViewModel

--- app/src/main/java/com/asap/aljyo/core/components/viewmodel/main/FilterViewModel.kt
+++ app/src/main/java/com/asap/aljyo/core/components/viewmodel/main/FilterViewModel.kt
@@ -2,7 +2,13 @@ package com.asap.aljyo.core.components.viewmodel.main
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.viewModelScope
 import com.asap.aljyo.core.components.viewmodel.NetworkViewModel
+import com.asap.aljyo.ui.UiState
+import com.asap.domain.entity.remote.AlarmGroup
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 sealed class Filter {
     data object Total: Filter()
@@ -18,7 +24,35 @@ abstract class FilterViewModel(
     private val _filterState = mutableStateOf<Filter>(Filter.Total)
     val filterState: State<Filter> get() = _filterState
 
-    fun select(filter: Filter) {
+    protected val originGroups = MutableStateFlow<List<AlarmGroup>>(emptyList())
+    protected val mGroupState = MutableStateFlow<UiState<List<AlarmGroup>>>(UiState.Loading)
+    val groupState get() = mGroupState.asStateFlow()
+
+    fun filter(filter: Filter) {
         _filterState.value = filter
+        viewModelScope.launch {
+            originGroups.run {
+                when (filter) {
+                    Filter.Private -> {
+                        mGroupState.emit(
+                            UiState.Success(
+                                value.filter { !it.isPublic }
+                            )
+                        )
+                    }
+
+                    Filter.Public -> {
+                        mGroupState.emit(
+                            UiState.Success(
+                                value.filter { it.isPublic }
+                            )
+                        )
+                    }
+                    Filter.Total -> {
+                        mGroupState.emit(UiState.Success(value))
+                    }
+                }
+            }
+        }
     }
 }

aa30312652f20e72bf15bf60e09e7fefc10a1916
dev-Jong <dev.whdgus090@gmail.com>
2025-03-17 22:12:01 +0900

    Fix: 계산기 알람 컨텐츠 정답 선택 시 문제 다시 생성

--- app/src/main/java/com/asap/aljyo/core/components/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/asap/aljyo/core/components/viewmodel/CalculatorViewModel.kt
@@ -7,7 +7,10 @@ import com.asap.utility.calculator.randomOperation
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -26,32 +29,38 @@ class CalculatorViewModel @Inject constructor(
 ) : AlarmOffViewModel(alarmOffUseCase) {
     private var solveCount = 0
 
+    // 계산기 문제 정보
     private val _operation = MutableStateFlow(randomOperation())
     val operation get() = _operation.asStateFlow()
 
+    // 답안지 리스트 정보
     private val _choiceState = MutableStateFlow(ChoiceState(index = -1))
     val choiceState get() = _choiceState.asStateFlow()
 
-    // 사용자 선택 이벤트 처리
+    // 사용자 답안지 선택 이벤트 처리
     fun emit(groupId: Int, index: Int, value: Int) {
         viewModelScope.launch {
+            // 답안지 선택 비활성화
             _choiceState.emit(_choiceState.value.disable(index))
-            val answer = _operation.value.isAnswer(value)
-            delay(
+            _operation.value.isAnswer(value).let { answer ->
+                delay(if (answer) 1000 else 2000)
                 if (answer) {
                     solveCount++
-                    1000
-                } else {
-                    2000
-                }
-            )
 
-            if (solveCount == ArithmeticOperation.GAME_COUNT) {
-                alarmOff(groupId)
-                return@launch
+                    if (solveCount == ArithmeticOperation.GAME_COUNT) {
+                        alarmOff(groupId)
+                        return@launch
+                    }
+                }
+                _choiceState.emit(_choiceState.value.enable())
+
+                if (answer) {
+                    // animation duration
+                    delay(200)
+                    // 새로운 문제 생성
+                    _operation.emit(randomOperation())
+                }
             }
-            _choiceState.emit(_choiceState.value.enable())
-            _operation.emit(randomOperation())
         }
     }
 }

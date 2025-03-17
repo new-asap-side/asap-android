81caf161590aa54a12af7aad88a189ec8e73e642
dev-Jong <dev.whdgus090@gmail.com>
2025-03-17 12:16:15 +0900

    Chore: MainViewModel import Log

--- app/src/main/java/com/asap/aljyo/core/components/viewmodel/main/MainViewModel.kt
+++ app/src/main/java/com/asap/aljyo/core/components/viewmodel/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.asap.aljyo.core.components.viewmodel.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.asap.domain.usecase.user.FetchProfileItemUseCase

a1efa4ab64915a61d27c598f4e70bc3efc7ce931
dev-Jong <dev.whdgus090@gmail.com>
2025-03-18 10:58:12 +0900

    Feat: GroupDetails 스크롤 시 top bar dim 처리

--- app/src/main/java/com/asap/aljyo/core/components/group_details/GroupDetailsViewModel.kt
+++ app/src/main/java/com/asap/aljyo/core/components/group_details/GroupDetailsViewModel.kt
@@ -29,7 +29,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import retrofit2.HttpException

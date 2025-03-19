fb2ea1a5c16d979a0449bddca7239984adebe3c8
dev-Jong <dev.whdgus090@gmail.com>
2025-03-19 14:21:47 +0900

    Feat: 오늘 알람 랭킹 usecase 추가

--- domain/src/main/java/com/asap/domain/usecase/group/GroupRankingUseCase.kt
+++ domain/src/main/java/com/asap/domain/usecase/group/GroupRankingUseCase.kt
@@ -6,21 +6,35 @@ import com.asap.domain.repository.GroupRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+data class GroupRankingUseCase(
+    val fetchGroupRankingUseCase: FetchGroupRankingUseCase,
+    val fetchRankingNumberUseCase: FetchRankingNumberUseCase,
+    val fetchTodayRankingUseCase: FetchTodayRankingUseCase
+)
+
+// 그룹 누적 랭킹 조회
 interface FetchGroupRankingUseCase {
+    suspend operator fun invoke(groupId: Int): Flow<List<GroupRanking>?>
+}
+
+// 그룹 알람 해제 랭킹 조회
+interface FetchRankingNumberUseCase {
+    suspend operator fun invoke(groupId: Int): Flow<RankingNumberResponse?>
+}
+
+// 그룹 일일 랭킹 조회
+interface FetchTodayRankingUseCase {
     suspend operator fun invoke(groupId: Int): Flow<List<GroupRanking>?>
 }
 
 class FetchGroupRankingUseCaseImpl @Inject constructor(
     private val groupRepository: GroupRepository
 ) : FetchGroupRankingUseCase {
+
     override suspend fun invoke(groupId: Int): Flow<List<GroupRanking>?> =
         groupRepository.fetchGroupRanking(groupId = groupId)
-
 }
 
-interface FetchRankingNumberUseCase {
-    suspend operator fun invoke(groupId: Int): Flow<RankingNumberResponse?>
-}
 
 class FetchRankingNumberUseCaseImpl @Inject constructor(
     private val groupRepository: GroupRepository

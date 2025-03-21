fa55da9215c71ff0672fd6f47597f53634a935e9
dev-Jong <dev.whdgus090@gmail.com>
2025-03-21 11:50:10 +0900

    Feat: MyRankingViewModel MyRankingSheetState 상태 관리 로직 추가

--- app/src/main/java/com/asap/aljyo/core/components/viewmodel/MyRankingViewModel.kt
+++ app/src/main/java/com/asap/aljyo/core/components/viewmodel/MyRankingViewModel.kt
@@ -2,32 +2,65 @@ package com.asap.aljyo.core.components.viewmodel
 
 import androidx.compose.runtime.Immutable
 import androidx.lifecycle.viewModelScope
+import com.asap.aljyo.ui.UiState
+import com.asap.domain.entity.remote.MyRanking
+import com.asap.domain.usecase.group.MyRankingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @Immutable
-data class MyRankingSheetState<T>(
+data class MyRankingSheetState(
     val show: Boolean,
-    val myRanks: List<T>
+    val myRanks: UiState<List<MyRanking>>
 )
 
 @HiltViewModel
-class MyRankingViewModel @Inject constructor() : NetworkViewModel() {
+class MyRankingViewModel @Inject constructor(
+    private val myRankingUseCase: MyRankingUseCase
+) : NetworkViewModel() {
     override val prefix: String = "MyRanking"
 
-    val fetchMyRankingFlow = flow {
-        emit(1)
+    private val _show = MutableStateFlow(false)
+    private val _myRankingState = MutableStateFlow<UiState<List<MyRanking>>>(UiState.Loading)
+
+    val sheetState = combine(_show, _myRankingState) { show, myRanking ->
+        MyRankingSheetState(show, myRanking)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = MyRankingSheetState(false, UiState.Loading)
+    )
+
+    fun fetchMyRanking() {
+        viewModelScope.launch(Dispatchers.Default) {
+            myRankingUseCase().catch { e ->
+                _myRankingState.emit(handleThrowable(e))
+            }.collect { result ->
+                _myRankingState.run {
+                    result?.let { myRanking ->
+                        emit(UiState.Success(myRanking))
+                    } ?: emit(UiState.Error(-1))
+                }
+            }
+        }
     }
 
-    val myRankingState = fetchMyRankingFlow.map {
+    fun showSheet() {
+        viewModelScope.launch {
+            _show.emit(true)
+        }
+    }
 
-    }.stateIn(
-        viewModelScope,
-        started = SharingStarted.WhileSubscribed(),
-        initialValue = 0
-    )
+    fun hideSheet() {
+        viewModelScope.launch {
+            _show.emit(false)
+        }
+    }
 }

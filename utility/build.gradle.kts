3d87fea919b5d9ad0c75e2c400f10f1de3ce7c81
dev-Jong <dev.whdgus090@gmail.com>
2025-04-02 23:26:39 +0900

    chore: add coroutine test dependency

--- utility/build.gradle.kts
+++ utility/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+    testImplementation(libs.coroutine.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 

439c432880e6f4eb756af8c8f6900846ea21a4a2
dev-Jong <dev.whdgus090@gmail.com>
2025-04-08 14:03:42 +0900

    feat: implement patchAlarmToken method (AuthRemoteDataSource)

--- data/src/main/java/com/asap/data/remote/datasource/AuthRemoteDataSource.kt
+++ data/src/main/java/com/asap/data/remote/datasource/AuthRemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.asap.data.remote.datasource
 
+import com.asap.data.remote.request.PatchAlarmTokenBody
 import com.asap.data.remote.service.AuthService
 import com.asap.domain.entity.remote.AuthKakaoBody
+import com.asap.domain.entity.remote.WhetherResponse
 import com.asap.domain.entity.remote.auth.AuthResponse
 import com.asap.domain.entity.remote.auth.RefreshTokenResponse
 import com.asap.domain.entity.remote.auth.TokenManager
@@ -13,6 +15,15 @@ import javax.inject.Inject
 class AuthRemoteDataSource @Inject constructor(
     private val authService: AuthService
 ) {
+    suspend fun patchAlarmToken(userId: Int, token: String): Flow<WhetherResponse?> = flow {
+        PatchAlarmTokenBody(userId = userId, alarmToken = token).let { body ->
+            authService.patchAlarmToken(body)
+        }.also { response ->
+            if (!response.isSuccessful) throw HttpException(response)
+            emit(response.body())
+        }
+    }
+
     suspend fun authKakao(kakaoAccessToken: String): Flow<AuthResponse?> = flow {
         val response = authService.authKakao(
             AuthKakaoBody(

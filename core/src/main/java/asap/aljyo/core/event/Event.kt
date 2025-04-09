8d17b27a8fd5f27663d77399db37df189a303d5e
dev-Jong <dev.whdgus090@gmail.com>
2025-04-09 17:55:02 +0900

    fix: Event interface process 반환 타입 정의

--- core/src/main/java/asap/aljyo/core/event/Event.kt
+++ core/src/main/java/asap/aljyo/core/event/Event.kt
@@ -2,6 +2,6 @@ package asap.aljyo.core.event
 
 
 // user interaction으로 발생하는 이벤트
-sealed interface Event<in T> {
-    suspend fun process(input: T)
+sealed interface Event<in I, out R> {
+    suspend fun process(input: I): R
 }

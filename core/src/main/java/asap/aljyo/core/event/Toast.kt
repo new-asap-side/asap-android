2cf92bc9e45616016cdac8a4b743013ebf7ee7b2
dev-Jong <dev.whdgus090@gmail.com>
2025-04-10 17:52:15 +0900

    fix: 기존 toast 함수 삭제

--- core/src/main/java/asap/aljyo/core/event/Toast.kt
+++ core/src/main/java/asap/aljyo/core/event/Toast.kt
@@ -3,10 +3,6 @@ package asap.aljyo.core.event
 import android.content.Context
 import android.widget.Toast
 
-fun shortToast(context: Context, content: String) {
-    Toast.makeText(context, content, Toast.LENGTH_SHORT).show()
-}
-
 fun Context.shortToast(content: String) =
     Toast.makeText(this, content, Toast.LENGTH_SHORT).show()
 

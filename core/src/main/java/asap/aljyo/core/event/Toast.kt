db3e0a44bdfc159bf3d971ec73de68d31f8779f6
dev-Jong <dev.whdgus090@gmail.com>
2025-04-10 17:17:00 +0900

    fix: Toast extension으로 변경

--- core/src/main/java/asap/aljyo/core/event/Toast.kt
+++ core/src/main/java/asap/aljyo/core/event/Toast.kt
@@ -6,3 +6,9 @@ import android.widget.Toast
 fun shortToast(context: Context, content: String) {
     Toast.makeText(context, content, Toast.LENGTH_SHORT).show()
 }
+
+fun Context.shortToast(content: String) =
+    Toast.makeText(this, content, Toast.LENGTH_SHORT).show()
+
+fun Context.longToast(content: String) =
+    Toast.makeText(this, content, Toast.LENGTH_LONG).show()

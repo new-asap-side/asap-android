36ca17a40c7f38da3e5fc0a357feff45a605f958
dev-Jong <dev.whdgus090@gmail.com>
2025-04-11 18:12:06 +0900

    refactor: Theme 신규 Typography 적용

--- presentation/src/main/java/asap/aljyo/presentation/theme/Theme.kt
+++ presentation/src/main/java/asap/aljyo/presentation/theme/Theme.kt
@@ -1,9 +1,12 @@
 package asap.aljyo.presentation.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 
 private val lightScheme = lightColorScheme(
     primary = AljyoColor.Red01,
@@ -13,14 +16,23 @@ private val lightScheme = lightColorScheme(
     onSurface = AljyoColor.Black01
 )
 
+private val darkScheme = darkColorScheme(
+    primary = AljyoColor.Red01,
+    primaryContainer = AljyoColor.Red02,
+    onPrimary = AljyoColor.White,
+    surface = AljyoColor.Grey01,
+    onSurface = AljyoColor.Black01
+)
+
 @Composable
 fun AljyoTheme(
-//    isDark: Boolean = isSystemInDarkTheme(),
+    isDark: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val colorScheme = if(isDark) darkScheme else lightScheme
     CompositionLocalProvider(LocalAljyoTypography provides independentTypography()) {
         MaterialTheme(
-            colorScheme = lightScheme,
+            colorScheme = colorScheme,
             content = content
         )
     }
@@ -29,5 +41,6 @@ fun AljyoTheme(
 object AljyoTheme {
     val typograpy: AljyoTypography
         @Composable
+        @ReadOnlyComposable
         get() = LocalAljyoTypography.current
 }

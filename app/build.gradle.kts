85c4d729fcf6bb95d50ae611ae4383fa69197b45
dev-Jong <dev.whdgus090@gmail.com>
2025-04-24 13:46:20 +0900

    Update: v1.0.8(9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.asap.aljyo"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
